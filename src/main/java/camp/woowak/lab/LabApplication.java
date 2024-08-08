f9f84837b842f3871134d13bf9d9b9a00201567b
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-08 15:45:37 +0900

    [build] 프로젝트 초기화
    기본적인 springboot 프로젝트 초기화

---
+++ src/main/java/camp/woowak/lab/LabApplication.java
@@ -0,0 +1,13 @@
+package camp.woowak.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabApplication.class, args);
+	}
+
+}
