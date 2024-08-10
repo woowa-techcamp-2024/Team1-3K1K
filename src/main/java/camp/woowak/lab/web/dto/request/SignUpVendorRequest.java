17fb832af0f5145cd55797ec33a6f1556f891f39
donghar <donghar@naver.com>
2024-08-10 23:05:10 +0900

    [feat] Phone 검증 기능
    전화번호를 검증하기 위한 어노테이션 및 커스텀 Validator 구현

--- src/main/java/camp/woowak/lab/web/dto/request/SignUpVendorRequest.java
+++ src/main/java/camp/woowak/lab/web/dto/request/SignUpVendorRequest.java
@@ -1,9 +1,18 @@
 package camp.woowak.lab.web.dto.request;
 
+import org.hibernate.validator.constraints.Length;
+
+import camp.woowak.lab.web.validation.annotation.Phone;
+import jakarta.validation.constraints.Email;
+
 public record SignUpVendorRequest(
+	@Length(min = 1, max = 50)
 	String name,
+	@Email
 	String email,
+	@Length(min = 1, max = 30)
 	String password,
+	@Phone
 	String phone
 ) {
 }
