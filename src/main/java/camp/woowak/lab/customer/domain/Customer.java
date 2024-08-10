e7bc581dc9bd7536208915dd5fd0f9ab856e9d12
donghar <donghar@naver.com>
2024-08-10 16:17:43 +0900

    [style] 코딩 컨벤션 적용
    개행문자를 space에서 tab으로 변경

--- src/main/java/camp/woowak/lab/customer/domain/Customer.java
+++ src/main/java/camp/woowak/lab/customer/domain/Customer.java
@@ -1,13 +1,18 @@
 package camp.woowak.lab.customer.domain;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    private PayAccount payAccount;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@OneToOne(fetch = FetchType.LAZY)
+	private PayAccount payAccount;
 }
--- src/main/java/camp/woowak/lab/store/domain/Store.java
+++ src/main/java/camp/woowak/lab/store/domain/Store.java
@@ -1,13 +1,18 @@
 package camp.woowak.lab.store.domain;
 
 import camp.woowak.lab.vendor.domain.Vendor;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Store {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Vendor owner;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Vendor owner;
 }
