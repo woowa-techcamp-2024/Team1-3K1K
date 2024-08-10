f93f83d940e5ea536ffcc7aefef9bea204cab80c
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-10 20:58:00 +0900

    [feat] AuthController.signUpCustomer 구현
    - 구매자 회원가입 API 구현

--- src/main/java/camp/woowak/lab/web/api/AuthController.java
+++ src/main/java/camp/woowak/lab/web/api/AuthController.java
@@ -8,10 +8,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import camp.woowak.lab.customer.service.SignUpCustomerService;
+import camp.woowak.lab.customer.service.command.SignUpCustomerCommand;
 import camp.woowak.lab.vendor.exception.DuplicateEmailException;
 import camp.woowak.lab.vendor.exception.InvalidCreationException;
 import camp.woowak.lab.vendor.service.SignUpVendorService;
 import camp.woowak.lab.vendor.service.command.SignUpVendorCommand;
+import camp.woowak.lab.web.dto.request.SignUpCustomerRequest;
 import camp.woowak.lab.web.dto.request.SignUpVendorRequest;
 import camp.woowak.lab.web.dto.response.ApiResponse;
 import camp.woowak.lab.web.error.ErrorCode;
@@ -20,9 +23,11 @@ import camp.woowak.lab.web.error.ErrorCode;
 @RequestMapping("/auth")
 public class AuthController {
 	private final SignUpVendorService signUpVendorService;
+	private final SignUpCustomerService signUpCustomerService;
 
-	public AuthController(SignUpVendorService signUpVendorService) {
+	public AuthController(SignUpVendorService signUpVendorService, SignUpCustomerService signUpCustomerService) {
 		this.signUpVendorService = signUpVendorService;
+		this.signUpCustomerService = signUpCustomerService;
 	}
 
 	@PostMapping("/vendors")
@@ -38,5 +43,20 @@ public class AuthController {
 			return ResponseEntity.ok(ApiResponse.error(ErrorCode.AUTH_DUPLICATE_EMAIL));
 		}
 		return ResponseEntity.created(URI.create("/vendors/" + registeredId)).build();
+	}
+
+	@PostMapping("/customers")
+	public ResponseEntity<?> signUpCustomer(@RequestBody SignUpCustomerRequest request) {
+		SignUpCustomerCommand command =
+			new SignUpCustomerCommand(request.name(), request.email(), request.password(), request.phone());
+		Long registeredId;
+		try {
+			registeredId = signUpCustomerService.signUp(command);
+		} catch (camp.woowak.lab.customer.exception.InvalidCreationException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(ErrorCode.SIGNUP_INVALID_REQUEST));
+		} catch (camp.woowak.lab.customer.exception.DuplicateEmailException e) {
+			return ResponseEntity.ok(ApiResponse.error(ErrorCode.AUTH_DUPLICATE_EMAIL));
+		}
+		return ResponseEntity.created(URI.create("/customers/" + registeredId)).build();
 	}
 }
