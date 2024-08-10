db202b2baabb39571dc44a3c51a799b3742f9f6f
donghar <donghar@naver.com>
2024-08-10 22:53:29 +0900

    [fix] Vendor 컨트롤러 변경
    `~` /auth/vendors -> /vendors
    `~` 예외처리 rfc7807에 부합하게 변경

---
+++ src/main/java/camp/woowak/lab/web/api/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package camp.woowak.lab.web.api;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
--- src/main/java/camp/woowak/lab/web/api/VendorController.java
+++ src/main/java/camp/woowak/lab/web/api/VendorController.java
@@ -2,10 +2,11 @@ package camp.woowak.lab.web.api;
 
 import java.net.URI;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import camp.woowak.lab.vendor.exception.DuplicateEmailException;
@@ -15,25 +16,27 @@ import camp.woowak.lab.vendor.service.command.SignUpVendorCommand;
 import camp.woowak.lab.web.dto.request.SignUpVendorRequest;
 import camp.woowak.lab.web.dto.response.ApiResponse;
 import camp.woowak.lab.web.error.ErrorCode;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/auth")
-public class AuthController {
+public class VendorController {
 	private final SignUpVendorService signUpVendorService;
 
-	public AuthController(SignUpVendorService signUpVendorService) {
+	public VendorController(SignUpVendorService signUpVendorService) {
 		this.signUpVendorService = signUpVendorService;
 	}
 
 	@PostMapping("/vendors")
-	public ResponseEntity<?> signUpVendor(@RequestBody SignUpVendorRequest request) {
+	public ResponseEntity<?> signUpVendor(@Valid @RequestBody SignUpVendorRequest request) {
 		SignUpVendorCommand command =
 			new SignUpVendorCommand(request.name(), request.email(), request.password(), request.phone());
 		Long registeredId;
 		try {
 			registeredId = signUpVendorService.signUp(command);
 		} catch (InvalidCreationException e) {
-			return ResponseEntity.badRequest().body(ApiResponse.error(ErrorCode.SIGNUP_INVALID_REQUEST));
+			return ResponseEntity.badRequest()
+				.body(ProblemDetail.forStatusAndDetail(
+					HttpStatus.BAD_REQUEST, ErrorCode.SIGNUP_INVALID_REQUEST.getMessage()));
 		} catch (DuplicateEmailException e) {
 			return ResponseEntity.ok(ApiResponse.error(ErrorCode.AUTH_DUPLICATE_EMAIL));
 		}
