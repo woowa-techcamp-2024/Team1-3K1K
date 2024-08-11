fb7ba6a404bacc9c9aec6393ed3c958a52b65710
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-11 20:47:20 +0900

    [feat] AuthenticationException - 구매자 로그인 실패 시 인증 오류 구현

---
+++ src/main/java/camp/woowak/lab/customer/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package camp.woowak.lab.customer.exception;
+
+public class AuthenticationException extends RuntimeException {
+	public AuthenticationException(String message) {
+		super(message);
+	}
+}
