d56b5331f115980ddef8f9c62793fe213440a6e9
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-11 21:04:27 +0900

    [feat] CustomerController.signIn 구현 - 구매자 로그인 성공 시 OK 반환 - 구매자 로그인 실패 시 BadRequest 반환

--- src/main/java/camp/woowak/lab/web/api/CustomerController.java
+++ src/main/java/camp/woowak/lab/web/api/CustomerController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import camp.woowak.lab.customer.exception.AuthenticationException;
+import camp.woowak.lab.customer.service.SignInCustomerService;
 import camp.woowak.lab.customer.service.SignUpCustomerService;
+import camp.woowak.lab.customer.service.command.SignInCustomerCommand;
 import camp.woowak.lab.customer.service.command.SignUpCustomerCommand;
+import camp.woowak.lab.web.dto.request.SignInCustomerRequest;
 import camp.woowak.lab.web.dto.request.SignUpCustomerRequest;
 import camp.woowak.lab.web.dto.response.ApiResponse;
 import camp.woowak.lab.web.error.ErrorCode;
@@ -17,9 +21,12 @@ import jakarta.validation.Valid;
 @RestController
 public class CustomerController {
 	private final SignUpCustomerService signUpCustomerService;
+	private final SignInCustomerService signInCustomerService;
 
-	public CustomerController(SignUpCustomerService signUpCustomerService) {
+	public CustomerController(SignUpCustomerService signUpCustomerService,
+							  SignInCustomerService signInCustomerService) {
 		this.signUpCustomerService = signUpCustomerService;
+		this.signInCustomerService = signInCustomerService;
 	}
 
 	@PostMapping("/customers")
@@ -35,5 +42,16 @@ public class CustomerController {
 			return ResponseEntity.ok(ApiResponse.error(ErrorCode.AUTH_DUPLICATE_EMAIL));
 		}
 		return ResponseEntity.created(URI.create("/customers/" + registeredId)).build();
+	}
+
+	@PostMapping("/customers/sign-in")
+	public ResponseEntity<?> signIn(@RequestBody SignInCustomerRequest request) {
+		try {
+			signInCustomerService.signIn(new SignInCustomerCommand(request.email(), request.password()));
+		} catch (AuthenticationException e) {
+			return ResponseEntity.badRequest().body(ApiResponse.error(ErrorCode.AUTH_INVALID_CREDENTIALS));
+		}
+		// TODO: JWT 토큰 발급
+		return ResponseEntity.ok().build();
 	}
 }
