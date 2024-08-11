6ae068b54e29947c09c109d448840ded36e1d5ae
june-777 <wlwhswnsrl96@gmail.com>
2024-08-12 01:05:18 +0900

    [feat] Store 예외 핸들러 구현

---
+++ src/main/java/camp/woowak/lab/web/api/store/StoreExceptionHandler.java
@@ -0,0 +1,21 @@
+package camp.woowak.lab.web.api.store;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import camp.woowak.lab.store.exception.StoreException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class StoreExceptionHandler {
+
+	@ExceptionHandler(StoreException.class)
+	public ResponseEntity<?> handleException(StoreException exception) {
+		log.warn(exception.getMessage(), exception);
+		return new ResponseEntity<>("fail", HttpStatus.BAD_REQUEST);
+	}
+	
+}
