a61c64d3cfa1795d54ec041bb9b6496338467f63
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-11 21:07:57 +0900

    [feat] 계좌 입출금의 히스토리에 대한 도메인 엔티티 생성
    - PayAccount를 외래키로 가지고 있음
     - 관계는 1:N
    - 생성 시간을 JPA에서 관리할 수 있도록 AuditingEntityListener 추가

---
+++ src/main/java/camp/woowak/lab/payaccount/domain/PayAccountHistory.java
@@ -0,0 +1,60 @@
+package camp.woowak.lab.payaccount.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@EntityListeners(value = AuditingEntityListener.class)
+public class PayAccountHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "account_id", nullable = false)
+	private PayAccount payAccount;
+
+	@Column
+	private long amount;
+
+	@Enumerated(value = EnumType.STRING)
+	private AccountTransactionType type;
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	public PayAccountHistory() {
+	}
+
+	public PayAccountHistory(PayAccount payAccount, long amount, AccountTransactionType type) {
+		this.payAccount = payAccount;
+		this.amount = amount;
+		this.type = type;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public AccountTransactionType getType() {
+		return type;
+	}
+}
