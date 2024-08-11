f0aa992eef613e2afc46da14104b8e773a2461e9
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-11 13:51:55 +0900

    [fix] SignUpCustomerRequest - email 필드에 @NotBlank 추가 - 이메일이 "" 이어도 통과되는 문제 수정

--- src/main/java/camp/woowak/lab/web/dto/request/SignUpCustomerRequest.java
+++ src/main/java/camp/woowak/lab/web/dto/request/SignUpCustomerRequest.java
@@ -4,10 +4,12 @@ import org.hibernate.validator.constraints.Length;
 
 import camp.woowak.lab.web.validation.annotation.Phone;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record SignUpCustomerRequest(
 	@Length(min = 1, max = 50)
 	String name,
+	@NotBlank
 	@Email
 	String email,
 	@Length(min = 8, max = 20)
