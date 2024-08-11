8b683826787c2e764fbe2babfb78d0a6280a43d4
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-11 21:11:27 +0900

    [feat] deposit과 withdraw service에 history를 받아 저장할 수 있는 기능 추가 구현
    - 입/출금 서비스는 항상 기록을 남겨야하기 때문에 추가

--- src/main/java/camp/woowak/lab/payaccount/service/PayAccountDepositService.java
+++ src/main/java/camp/woowak/lab/payaccount/service/PayAccountDepositService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
+import camp.woowak.lab.payaccount.domain.PayAccountHistory;
 import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
+import camp.woowak.lab.payaccount.repository.PayAccountHistoryRepository;
 import camp.woowak.lab.payaccount.repository.PayAccountRepository;
 import camp.woowak.lab.payaccount.service.command.AccountTransactionCommand;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PayAccountDepositService {
 	private final PayAccountRepository payAccountRepository;
+	private final PayAccountHistoryRepository payAccountHistoryRepository;
 
-	public PayAccountDepositService(PayAccountRepository payAccountRepository) {
+	public PayAccountDepositService(PayAccountRepository payAccountRepository,
+									PayAccountHistoryRepository payAccountHistoryRepository) {
 		this.payAccountRepository = payAccountRepository;
+		this.payAccountHistoryRepository = payAccountHistoryRepository;
 	}
 
 	@Transactional
@@ -23,8 +28,9 @@ public class PayAccountDepositService {
 		PayAccount payAccount = payAccountRepository.findByIdForUpdate(command.payAccountId())
 			.orElseThrow(() -> new NotFoundAccountException("Invalid account id with " + command.payAccountId()));
 
-		payAccount.deposit(command.amount());
+		PayAccountHistory depositHistory = payAccount.deposit(command.amount());
 		log.info("A deposit of {} has been completed into Account ID {}.", command.amount(), command.payAccountId());
+		payAccountHistoryRepository.save(depositHistory);
 
 		return payAccount.getBalance();
 	}
--- src/main/java/camp/woowak/lab/payaccount/service/PayAccountWithdrawService.java
+++ src/main/java/camp/woowak/lab/payaccount/service/PayAccountWithdrawService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
+import camp.woowak.lab.payaccount.domain.PayAccountHistory;
 import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
+import camp.woowak.lab.payaccount.repository.PayAccountHistoryRepository;
 import camp.woowak.lab.payaccount.repository.PayAccountRepository;
 import camp.woowak.lab.payaccount.service.command.AccountTransactionCommand;
 import lombok.extern.slf4j.Slf4j;
@@ -13,9 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PayAccountWithdrawService {
 	private final PayAccountRepository payAccountRepository;
+	private final PayAccountHistoryRepository payAccountHistoryRepository;
 
-	public PayAccountWithdrawService(PayAccountRepository payAccountRepository) {
+	public PayAccountWithdrawService(PayAccountRepository payAccountRepository,
+									 PayAccountHistoryRepository payAccountHistoryRepository) {
 		this.payAccountRepository = payAccountRepository;
+		this.payAccountHistoryRepository = payAccountHistoryRepository;
 	}
 
 	@Transactional
@@ -23,8 +28,9 @@ public class PayAccountWithdrawService {
 		PayAccount payAccount = payAccountRepository.findByIdForUpdate(command.payAccountId())
 			.orElseThrow(() -> new NotFoundAccountException("Invalid account id with " + command.payAccountId()));
 
-		payAccount.withdraw(command.amount());
+		PayAccountHistory withdrawHistory = payAccount.withdraw(command.amount());
 		log.info("A withdrawal of {} has been completed from Account ID {}", command.amount(), command.payAccountId());
+		payAccountHistoryRepository.save(withdrawHistory);
 
 		return payAccount.getBalance();
 	}
