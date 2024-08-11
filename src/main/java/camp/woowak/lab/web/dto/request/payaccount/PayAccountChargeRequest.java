f34aed0264396530838b60ed4970a14fb28f2df3
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-11 22:28:11 +0900

    [chore] PayAccountChargeRequest의 format 수정
    공통 포멧으로 재 적용

--- src/main/java/camp/woowak/lab/web/dto/request/payaccount/PayAccountChargeRequest.java
+++ src/main/java/camp/woowak/lab/web/dto/request/payaccount/PayAccountChargeRequest.java
@@ -3,7 +3,7 @@ package camp.woowak.lab.web.dto.request.payaccount;
 import jakarta.validation.constraints.NotNull;
 
 public record PayAccountChargeRequest(
-	@NotNull(message="amount can't be null")
+	@NotNull(message = "amount can't be null")
 	Long amount
 ) {
 }
