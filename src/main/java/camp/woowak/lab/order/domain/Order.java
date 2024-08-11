790b465ec432bc61644787229991f2d7aacf6a16
june-777 <wlwhswnsrl96@gmail.com>
2024-08-11 22:17:19 +0900

    [fix] Order 엔티티의 테이블 이름을 Orders 로 명시
    order 는 DBMS 예약어라서 Auto DDL 과정에서 예외가 발생함

--- src/main/java/camp/woowak/lab/order/domain/Order.java
+++ src/main/java/camp/woowak/lab/order/domain/Order.java
@@ -2,15 +2,26 @@ package camp.woowak.lab.order.domain;
 
 import camp.woowak.lab.customer.domain.Customer;
 import camp.woowak.lab.store.domain.Store;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Orders")
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Customer requester;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Store store;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Customer requester;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Store store;
+
 }
