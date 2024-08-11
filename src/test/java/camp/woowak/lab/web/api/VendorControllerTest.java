8d52b59da009ae9d04d58c2c270aaf0a37da5d16
donghar <donghar@naver.com>
2024-08-11 14:57:05 +0900

    [test] 파리미터가 비어있는 Vendor 회원가입 요청
    파라미터가 오지 않은 경우 또는 비어있는 경우에 대한 테스트 추가

--- src/test/java/camp/woowak/lab/web/api/VendorControllerTest.java
+++ src/test/java/camp/woowak/lab/web/api/VendorControllerTest.java
@@ -60,56 +60,308 @@ class VendorControllerTest {
 				.andDo(print());
 		}
 
-		@Test
-		@DisplayName("[실패] 400 : 이름이 비어있는 경우")
-		void failWithInvalidName() throws Exception {
-			long fakeVendorId = new Random().nextLong(1000L);
-			BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
-				.willReturn(fakeVendorId);
+		@Nested
+		@DisplayName("[실패] 400")
+		class FailWith400 {
+			@Nested
+			@DisplayName("이름이")
+			class NameMust {
+				@Test
+				@DisplayName("비어있는 경우")
+				void failWithEmptyName() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
 
-			// when
-			ResultActions actions = mockMvc.perform(
-				post("/vendors")
-					.content(new ObjectMapper().writeValueAsString(
-						new SignUpVendorRequest("", "validEmail@validEmail.com", "validPassword",
-							"010-0000-0000")))
-					.accept(MediaType.APPLICATION_JSON)
-					.contentType(MediaType.APPLICATION_JSON)
-			);
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest(null, "validEmail@validEmail.com", "validPassword",
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
 
-			// then
-			actions.andExpect(status().isBadRequest())
-				.andExpect(jsonPath("$.type").value("about:blank"))
-				.andExpect(jsonPath("$.title").value("Bad Request"))
-				.andExpect(jsonPath("$.status").value(400))
-				.andExpect(jsonPath("$.instance").value("/vendors"))
-				.andDo(print());
-		}
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
 
-		@Test
-		@DisplayName("[실패] 400 : 불가능한 전화번호")
-		void failWithInvalidPhone() throws Exception {
-			long fakeVendorId = new Random().nextLong(1000L);
-			BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
-				.willReturn(fakeVendorId);
+				@Test
+				@DisplayName("공란인 경우")
+				void failWithBlankName() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
 
-			// when
-			ResultActions actions = mockMvc.perform(
-				post("/vendors")
-					.content(new ObjectMapper().writeValueAsString(
-						new SignUpVendorRequest("", "validEmail@validEmail.com", "validPassword",
-							"111-1111-0000")))
-					.accept(MediaType.APPLICATION_JSON)
-					.contentType(MediaType.APPLICATION_JSON)
-			);
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("", "validEmail@validEmail.com", "validPassword",
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
 
-			// then
-			actions.andExpect(status().isBadRequest())
-				.andExpect(jsonPath("$.type").value("about:blank"))
-				.andExpect(jsonPath("$.title").value("Bad Request"))
-				.andExpect(jsonPath("$.status").value(400))
-				.andExpect(jsonPath("$.instance").value("/vendors"))
-				.andDo(print());
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+			}
+
+			@Nested
+			@DisplayName("이메일이")
+			class EmailMust {
+				@Test
+				@DisplayName("비어있는 경우")
+				void failWithEmptyEmail() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", null, "validPassword", "010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("공란인 경우")
+				void failWithBlankEmail() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "", "validPassword", "010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+			}
+
+			@Nested
+			@DisplayName("비밀번호가")
+			class PasswordMust {
+				@Test
+				@DisplayName("비어있는 경우")
+				void failWithEmptyPassword() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com", null,
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("공란인 경우")
+				void failWithBlankPassword() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com", "",
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("8자 미만인 경우")
+				void failWith7Password() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com", "abcdefg",
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("30자 초과인 경우")
+				void failWith31Password() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com",
+									"aaaaaaaaaabbbbbbbbbbccccccccccd",
+									"010-0000-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+			}
+
+			@Nested
+			@DisplayName("전화번호가")
+			class PhoneMust {
+				@Test
+				@DisplayName("비어있는 경우")
+				void failWithEmptyPhone() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com", "validPassword",
+									null)))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("공란인 경우")
+				void failWithBlankPhone() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("validName", "validEmail@validEmail.com", "validPassword", "")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+
+				@Test
+				@DisplayName("잘못된 형식인 경우")
+				void failWithInvalidPhone() throws Exception {
+					long fakeVendorId = new Random().nextLong(1000L);
+					BDDMockito.given(signUpVendorService.signUp(BDDMockito.any(SignUpVendorCommand.class)))
+						.willReturn(fakeVendorId);
+
+					// when
+					ResultActions actions = mockMvc.perform(
+						post("/vendors")
+							.content(new ObjectMapper().writeValueAsString(
+								new SignUpVendorRequest("", "validEmail@validEmail.com", "validPassword",
+									"111-1111-0000")))
+							.accept(MediaType.APPLICATION_JSON)
+							.contentType(MediaType.APPLICATION_JSON)
+					);
+
+					// then
+					actions.andExpect(status().isBadRequest())
+						.andExpect(jsonPath("$.type").value("about:blank"))
+						.andExpect(jsonPath("$.title").value("Bad Request"))
+						.andExpect(jsonPath("$.status").value(400))
+						.andExpect(jsonPath("$.instance").value("/vendors"))
+						.andDo(print());
+				}
+			}
 		}
 
 		@Test
