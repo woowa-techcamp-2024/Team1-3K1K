33f252298d976616a147d6d3391c64f14ce4b149
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-11 12:17:18 +0900

    [feat] PayAccount에 x-lock을 위한 메서드 추가
    - 계좌 잔고와 같은 민감한 데이터는 x-lock을 통해 lock을 걸고 업데이트해야하기 때문에 추가

--- src/main/java/camp/woowak/lab/payaccount/repository/PayAccountRepository.java
+++ src/main/java/camp/woowak/lab/payaccount/repository/PayAccountRepository.java
@@ -1,8 +1,17 @@
 package camp.woowak.lab.payaccount.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
+import jakarta.persistence.LockModeType;
 
 public interface PayAccountRepository extends JpaRepository<PayAccount, Long> {
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	@Query("SELECT pa FROM PayAccount pa where pa.id = :id")
+	Optional<PayAccount> findByIdForUpdate(@Param("id") Long id);
 }
