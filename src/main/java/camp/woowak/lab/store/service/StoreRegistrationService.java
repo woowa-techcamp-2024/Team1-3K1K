3f2609ee116c47e9d570ad442dc506d083f0ee5f
june-777 <wlwhswnsrl96@gmail.com>
2024-08-11 22:12:56 +0900

    [feat] 가게 생성을 담당하는 서비스 클래스 구현
    - 발생가능한 RunTimeException 은 javadocs 로 명시

---
+++ src/main/java/camp/woowak/lab/store/service/StoreRegistrationService.java
@@ -0,0 +1,46 @@
+package camp.woowak.lab.store.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import camp.woowak.lab.store.domain.Store;
+import camp.woowak.lab.store.domain.StoreCategory;
+import camp.woowak.lab.store.exception.StoreException;
+import camp.woowak.lab.store.repository.StoreCategoryRepository;
+import camp.woowak.lab.store.repository.StoreRepository;
+import camp.woowak.lab.vendor.domain.Vendor;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class StoreRegistrationService {
+
+	private final StoreRepository storeRepository;
+	private final StoreCategoryRepository storeCategoryRepository;
+
+	/**
+	 * @throws StoreException Store 객체 검증 실패, 존재하지 않는 이름의 가게 카테고리
+	 */
+	@Transactional
+	public void storeRegistration(final Vendor vendor, final StoreRegistrationRequest request) {
+		final StoreCategory storeCategory = findStoreCategoryByName(request.storeCategoryName());
+		final Store store = createStore(vendor, storeCategory, request);
+
+		storeRepository.save(store);
+	}
+
+	private static Store createStore(Vendor vendor, StoreCategory storeCategory, StoreRegistrationRequest request) {
+		return new Store(vendor, storeCategory, request.storeName(),
+			request.storeAddress(),
+			request.storePhoneNumber(),
+			request.storeMinOrderPrice(),
+			request.storeStarTime(),
+			request.storeEndTime());
+	}
+
+	private StoreCategory findStoreCategoryByName(final String name) {
+		return storeCategoryRepository.findByName(name)
+			.orElseThrow(() -> new StoreException("존재하지 않는 가게 카테고리입니다."));
+	}
+
+}
