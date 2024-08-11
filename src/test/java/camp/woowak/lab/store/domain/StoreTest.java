aa47da4e2b494512a89399d0685e83293a0f32a5
june-777 <wlwhswnsrl96@gmail.com>
2024-08-11 20:24:48 +0900

    [test] Store 도메인 객체 생성 테스트 - 가게 이용 시간

--- src/test/java/camp/woowak/lab/store/domain/StoreTest.java
+++ src/test/java/camp/woowak/lab/store/domain/StoreTest.java
@@ -86,6 +86,119 @@ class StoreTest {
 			}
 		}
 
+		@Nested
+		@DisplayName("가게 이용 시간이")
+		class StoreTime {
+
+			@Test
+			@DisplayName("[Success] 시작 시간이 종료 시간보다 이전이다")
+			void storeStartTimeBeforeThanEndTime() {
+				// given
+				LocalDateTime validStartTime = validStartTimeFixture;
+				LocalDateTime validEndTime = validEndTimeFixture;
+
+				// when & then
+				assertThatCode(
+					() -> new Store(null, null, validNameFixture, validAddressFixture, null, validMinOrderPriceFixture,
+						validStartTime, validEndTime))
+					.doesNotThrowAnyException();
+			}
+
+			@Test
+			@DisplayName("[Exception] 종료 시간이 시작 시간과 같으면 StoreException 이 발생한다")
+			void endTimeSameWithStartTime() {
+				// given
+				LocalDateTime endTimeSameWithStartTime = fixedStartTime.now();
+
+				// when & then
+				assertThatThrownBy(
+					() -> new Store(null, null, validNameFixture, validAddressFixture, null, validMinOrderPriceFixture,
+						validStartTimeFixture, endTimeSameWithStartTime))
+					.isInstanceOf(StoreException.class);
+			}
+
+			@Test
+			@DisplayName("[Exception] 종료 시간이 시작 시간 이전이면 StoreException 이 발생한다")
+			void endTimeBeforeThanStartTime() {
+				// given
+				LocalDateTime endTimeBeforeThanStartTime = validStartTimeFixture.minusMinutes(1);
+
+				// when & then
+				assertThatThrownBy(
+					() -> new Store(null, null, validNameFixture, validAddressFixture, null, validMinOrderPriceFixture,
+						validStartTimeFixture, endTimeBeforeThanStartTime))
+					.isInstanceOf(StoreException.class);
+			}
+
+			@Test
+			@DisplayName("[Success] 시작 시간과 종료 시간의 단위는 분(m) 단위 까지 가능하다")
+			void validStartTimeUnit() {
+				// given
+				LocalDateTime validStartTimeUnitMinute = validStartTimeFixture;
+				LocalDateTime validEndTimeUnitMinute = validEndTimeFixture;
+
+				// when & then
+				assertThatCode(
+					() -> new Store(null, null, validNameFixture, validAddressFixture, null, validMinOrderPriceFixture,
+						validStartTimeUnitMinute, validEndTimeUnitMinute))
+					.doesNotThrowAnyException();
+			}
+
+			@Test
+			@DisplayName("[Exception] 시작 시간에 초(s) 단위가 포함되면 StoreException 이 발생한다")
+			void startTimeWithSeconds() {
+				// given
+				DateTimeProvider inValidUnitDateTimeProvider = () -> LocalDateTime.of(2024, 8, 24, 10, 10, 1);
+				LocalDateTime startTimeWithSeconds = inValidUnitDateTimeProvider.now();
+
+				// when & then
+				assertThatThrownBy(
+					() -> new Store(null, null, validNameFixture, validAddressFixture, null, validMinOrderPriceFixture,
+						startTimeWithSeconds, validEndTimeFixture))
+					.isInstanceOf(StoreException.class);
+			}
+
+			@Test
+			@DisplayName("[Exception] 시작 시간에 나노초(ms) 단위가 포함되면 StoreException 이 발생한다")
+			void startTimeWithNanoSeconds() {
+				// given
+				DateTimeProvider inValidUnitDateTimeProvider = () -> LocalDateTime.of(2024, 8, 24, 10, 0, 0, 1);
+				LocalDateTime startTimeWithNanoSeconds = inValidUnitDateTimeProvider.now();
+
+				// when & then
+				assertThatThrownBy(() -> new Store(null, null, validNameFixture, validAddressFixture, null, 5000,
+					startTimeWithNanoSeconds, validEndTimeFixture))
+					.isInstanceOf(StoreException.class);
+			}
+
+			@Test
+			@DisplayName("[Exception] 종료 시간에 초(s) 단위가 포함되면 StoreException 이 발생한다")
+			void endTimeWithSeconds() {
+				// given
+				DateTimeProvider inValidUnitDateTimeProvider = () -> LocalDateTime.of(2024, 8, 24, 10, 30, 1);
+				LocalDateTime endTimeWithSeconds = inValidUnitDateTimeProvider.now();
+
+				// when & then
+				assertThatThrownBy(() -> new Store(null, null, validNameFixture, validAddressFixture, null, 5000,
+					validStartTimeFixture, endTimeWithSeconds))
+					.isInstanceOf(StoreException.class);
+			}
+
+			@Test
+			@DisplayName("[Exception] 종료 시간에 나노초(ms) 단위가 포함되면 StoreException 이 발생한다")
+			void endTimeWithNanoSeconds() {
+				// given
+				DateTimeProvider inValidUnitDateTimeProvider = () -> LocalDateTime.of(2024, 8, 24, 10, 30, 0, 1);
+				LocalDateTime endTimeWithNanoSeconds = inValidUnitDateTimeProvider.now();
+
+				// when & then
+				assertThatThrownBy(() -> new Store(null, null, validNameFixture, validAddressFixture, null, 5000,
+					validStartTimeFixture, endTimeWithNanoSeconds))
+					.isInstanceOf(StoreException.class);
+			}
+
+		}
+
 	}
 
 }
