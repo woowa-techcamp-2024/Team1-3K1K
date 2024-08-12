e4cb60d7e9648a1e81d43c59debcb785e91cb456
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-13 00:52:48 +0900

    [feat] PayAccountChargeService 구현
    - 기존 입/출금 service와 charge를 구분하기 위해서 새로운 charge service 구현

---
+++ src/main/java/camp/woowak/lab/payaccount/service/PayAccountChargeService.java
@@ -0,0 +1,32 @@
+package camp.woowak.lab.payaccount.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import camp.woowak.lab.payaccount.domain.PayAccount;
+import camp.woowak.lab.payaccount.domain.PayAccountHistory;
+import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
+import camp.woowak.lab.payaccount.repository.PayAccountRepository;
+import camp.woowak.lab.payaccount.service.command.AccountTransactionCommand;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class PayAccountChargeService {
+	private final PayAccountRepository payAccountRepository;
+
+	public PayAccountChargeService(PayAccountRepository payAccountRepository) {
+		this.payAccountRepository = payAccountRepository;
+	}
+
+	@Transactional
+	public long chargeAccount(AccountTransactionCommand command) {
+		PayAccount payAccount = payAccountRepository.findByIdForUpdate(command.payAccountId())
+			.orElseThrow(() -> new NotFoundAccountException("Invalid account id with " + command.payAccountId()));
+
+		PayAccountHistory chargeHistory = payAccount.charge(command.amount());
+		log.info("A Charge of {} has been completed from Account ID {}", command.amount(), command.payAccountId());
+
+		return payAccount.getBalance();
+	}
+}
