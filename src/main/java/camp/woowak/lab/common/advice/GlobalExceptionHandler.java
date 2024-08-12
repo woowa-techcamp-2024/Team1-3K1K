f9073b5c75b872fb7c73a2b919fba60416de3812
donghar <donghar@naver.com>
2024-08-13 00:41:08 +0900

    [fix] 전역 ExceptionHandler 수정
    `-` 기존에 등록된 ExceptionHandler
    `~` 새로운 ExceptionHandler에 ResponseEntityExceptionHandler 상속

--- src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
+++ src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
@@ -5,12 +5,13 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ProblemDetail> handleAllUncaughtException(Exception e) {
--- src/main/java/camp/woowak/lab/web/api/GlobalExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package camp.woowak.lab.web.api;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-}
