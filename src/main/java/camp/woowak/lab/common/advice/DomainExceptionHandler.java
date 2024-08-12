6d08742adb007b339805d28cb50be5fb5c3cb395
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-12 22:01:48 +0900

    [refactor] CustomRestControllerAdvice 이름 변경 - DomainExceptionHandler 로 변경했습니다.

--- src/main/java/camp/woowak/lab/common/advice/DomainExceptionHandler.java
+++ src/main/java/camp/woowak/lab/common/advice/DomainExceptionHandler.java
@@ -1,18 +1,23 @@
 package camp.woowak.lab.common.advice;
 
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.lang.annotation.*;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @RestControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE - 1)
-public @interface CustomRestControllerAdvice {
+public @interface DomainExceptionHandler {
 	@AliasFor(annotation = RestControllerAdvice.class, attribute = "basePackages")
 	String[] basePackages() default {};
 
