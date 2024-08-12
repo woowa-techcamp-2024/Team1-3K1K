819b08d7d61da0333a2c66b21f3d467ef27058e7
donghar <donghar@naver.com>
2024-08-13 00:44:56 +0900

    [fix] Vandor 도메인에서 검증 로직 분리
    검증 로직은 VendorValidator에서 수행

--- src/main/java/camp/woowak/lab/vendor/domain/Vendor.java
+++ src/main/java/camp/woowak/lab/vendor/domain/Vendor.java
@@ -1,7 +1,7 @@
 package camp.woowak.lab.vendor.domain;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
-import camp.woowak.lab.vendor.exception.InvalidCreationException;
+import camp.woowak.lab.vendor.exception.InvalidVendorCreationException;
 import camp.woowak.lab.web.authentication.PasswordEncoder;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -30,13 +30,13 @@ public class Vendor {
 	protected Vendor() {
 	}
 
+	/**
+	 *
+	 * @throws InvalidVendorCreationException 검증에 실패하면
+	 */
 	public Vendor(String name, String email, String password, String phone, PayAccount payAccount,
-		PasswordEncoder passwordEncoder) throws InvalidCreationException {
-		checkName(name);
-		checkEmail(email);
-		checkPassword(password);
-		checkPhone(phone);
-		checkPayAccount(payAccount);
+		PasswordEncoder passwordEncoder) {
+		VendorValidator.validate(name, email, password, phone, payAccount);
 		this.name = name;
 		this.email = email;
 		this.password = passwordEncoder.encode(password);
@@ -46,48 +46,5 @@ public class Vendor {
 
 	public Long getId() {
 		return id;
-	}
-
-	private void checkPayAccount(PayAccount payAccount) throws InvalidCreationException {
-		if (payAccount == null) {
-			throw new InvalidCreationException("Pay account cannot be null");
-		}
-	}
-
-	private void checkPhone(String phone) throws InvalidCreationException {
-		if (phone == null || phone.isBlank()) {
-			throw new InvalidCreationException("Vendor phone cannot be blank");
-		}
-		if (phone.trim().length() > 30) {
-			throw new InvalidCreationException("Vendor phone cannot be longer than 30 characters");
-		}
-	}
-
-	private void checkPassword(String password) throws InvalidCreationException {
-		if (password == null || password.isBlank()) {
-			throw new InvalidCreationException("Vendor password cannot be blank");
-		}
-		if (password.trim().length() < 8 || password.trim().length() > 30) {
-			throw new InvalidCreationException(
-				"Vendor password must be at least 8 characters and at most 30 characters");
-		}
-	}
-
-	private void checkEmail(String email) throws InvalidCreationException {
-		if (email == null || email.isBlank()) {
-			throw new InvalidCreationException("Vendor email cannot be blank");
-		}
-		if (email.trim().length() > 100) {
-			throw new InvalidCreationException("Vendor email cannot be longer than 100 characters");
-		}
-	}
-
-	private void checkName(String name) throws InvalidCreationException {
-		if (name == null || name.isBlank()) {
-			throw new InvalidCreationException("Vendor name cannot be blank");
-		}
-		if (name.length() > 50) {
-			throw new InvalidCreationException("Vendor name cannot exceed 50 characters");
-		}
 	}
 }
