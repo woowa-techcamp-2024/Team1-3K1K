4910979a5ed5bceda0c406c6dbfdebb1d0aaccd9
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-12 21:23:02 +0900

    [chore] 사용하지 않는 import 제거

--- src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
+++ src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
@@ -1,12 +1,9 @@
 package camp.woowak.lab.common.advice;
 
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
