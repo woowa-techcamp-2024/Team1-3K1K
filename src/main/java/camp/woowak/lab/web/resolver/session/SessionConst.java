b86ed02ed9da2359bca7cb16f25e0ab71c3178db
donghar <donghar@naver.com>
2024-08-12 20:17:07 +0900

    [feat] 세션 키를 저장할 상수 클래스

---
+++ src/main/java/camp/woowak/lab/web/resolver/session/SessionConst.java
@@ -0,0 +1,9 @@
+package camp.woowak.lab.web.resolver.session;
+
+public final class SessionConst {
+	public static final String SESSION_VENDOR_KEY = "authentication_vendor";
+	public static final String SESSION_CUSTOMER_KEY = "authentication_customer";
+
+	private SessionConst() {
+	}
+}
