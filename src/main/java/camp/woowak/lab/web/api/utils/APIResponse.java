8c9a2229a0a45857433cd459500991f639992d75
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-12 21:06:17 +0900

    [docs] APIResponse에 사용시 주의사항 주석 추가
    - Jackson의 ObjectMapper와 함께 사용하기 위해서는 data로 들어오는 변수에 Getter가 필수적으로 있어야한다는 내용을 주석으로 추가

--- src/main/java/camp/woowak/lab/web/api/utils/APIResponse.java
+++ src/main/java/camp/woowak/lab/web/api/utils/APIResponse.java
@@ -2,6 +2,10 @@ package camp.woowak.lab.web.api.utils;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * APIResponse를 Jackson의 ObjectMapper와 함께 사용하려면,
+ * Generic Type의 {@code data}에는 Getter 메서드가 필요합니다.
+ */
 public class APIResponse<T> {
 	private final T data;
 	private final int status;
