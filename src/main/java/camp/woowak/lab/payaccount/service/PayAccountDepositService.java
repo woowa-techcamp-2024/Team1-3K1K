ec96339b74c088ead7d757a7fa075d1eca771297
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-13 00:46:25 +0900

    [refactor] domain에서 history를 저장하기 때문에 service 레이어에서 history를 저장하는 로직 삭제
    - history를 저장하는 책임을 도메인에게 넘겨버렸기 때문에 필요가 없어졌습니다.

--- src/main/java/camp/woowak/lab/payaccount/service/PayAccountDepositService.java
+++ src/main/java/camp/woowak/lab/payaccount/service/PayAccountDepositService.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import camp.woowak.lab.payaccount.domain.PayAccount;
 import camp.woowak.lab.payaccount.domain.PayAccountHistory;
 import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
-import camp.woowak.lab.payaccount.repository.PayAccountHistoryRepository;
 import camp.woowak.lab.payaccount.repository.PayAccountRepository;
 import camp.woowak.lab.payaccount.service.command.AccountTransactionCommand;
 import lombok.extern.slf4j.Slf4j;
@@ -15,12 +14,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PayAccountDepositService {
 	private final PayAccountRepository payAccountRepository;
-	private final PayAccountHistoryRepository payAccountHistoryRepository;
 
-	public PayAccountDepositService(PayAccountRepository payAccountRepository,
-									PayAccountHistoryRepository payAccountHistoryRepository) {
+	public PayAccountDepositService(PayAccountRepository payAccountRepository) {
 		this.payAccountRepository = payAccountRepository;
-		this.payAccountHistoryRepository = payAccountHistoryRepository;
 	}
 
 	@Transactional
@@ -30,7 +26,6 @@ public class PayAccountDepositService {
 
 		PayAccountHistory depositHistory = payAccount.deposit(command.amount());
 		log.info("A deposit of {} has been completed into Account ID {}.", command.amount(), command.payAccountId());
-		payAccountHistoryRepository.save(depositHistory);
 
 		return payAccount.getBalance();
 	}
