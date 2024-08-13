f932caeb697222a2f75f2293538c981ef6d0603a
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-13 14:20:38 +0900

    [feat] 에러코드 정책 변경에 따른 수정

--- src/main/java/camp/woowak/lab/customer/domain/CustomerValidator.java
+++ src/main/java/camp/woowak/lab/customer/domain/CustomerValidator.java
@@ -7,7 +7,7 @@ import camp.woowak.lab.payaccount.domain.PayAccount;
 public class CustomerValidator {
 
 	public static void validateCreation(String name, String email, String password, String phone,
-										PayAccount payAccount) throws InvalidCreationException {
+		PayAccount payAccount) throws InvalidCreationException {
 		validateName(name);
 		validateEmail(email);
 		validatePassword(password);
@@ -17,43 +17,48 @@ public class CustomerValidator {
 
 	public static void validateName(String name) throws InvalidCreationException {
 		if (name == null || name.isBlank()) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_NAME_IS_NOT_BLANK);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION, "Customer name cannot be blank");
 		}
 		if (name.length() > 50) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_NAME_IS_TOO_LONG);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION,
+				"Customer name cannot be longer than 50 characters");
 		}
 	}
 
 	public static void validateEmail(String email) throws InvalidCreationException {
 		if (email == null || email.isBlank()) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_EMAIL_IS_NOT_BLANK);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION, "Customer email cannot be blank");
 		}
 		if (email.trim().length() > 100) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_EMAIL_IS_TOO_LONG);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION,
+				"Customer email cannot be longer than 100 characters");
 		}
 	}
 
 	public static void validatePassword(String password) throws InvalidCreationException {
 		if (password == null || password.isBlank()) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_PASSWORD_IS_NOT_BLANK);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION, "Customer password cannot be blank");
 		}
 		if (password.trim().length() > 30) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_PASSWORD_IS_TOO_LONG);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION,
+				"Customer password cannot be longer than 30 characters");
 		}
 	}
 
 	public static void validatePhone(String phone) throws InvalidCreationException {
 		if (phone == null || phone.isBlank()) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_PHONE_IS_NOT_BLANK);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION, "Customer phone cannot be blank");
 		}
 		if (phone.trim().length() > 30) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_PHONE_IS_TOO_LONG);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION,
+				"Customer phone cannot be longer than 30 characters");
 		}
 	}
 
 	public static void validatePayAccount(PayAccount payAccount) throws InvalidCreationException {
 		if (payAccount == null) {
-			throw new InvalidCreationException(CustomerErrorCode.INVALID_PAY_ACCOUNT_IS_NOT_NULL);
+			throw new InvalidCreationException(CustomerErrorCode.INVALID_CREATION,
+				"Customer payAccount cannot be null");
 		}
 	}
 }
