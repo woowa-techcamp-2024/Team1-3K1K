e81511de029dba5b83815282f75c75ccc3741b62
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-13 09:19:34 +0900

    [refactor] CustomerController 의 에러 처리 주체 변경 - CustomerController 의 에러처리를 CustomerExceptionHandler 에 위임

--- src/main/java/camp/woowak/lab/web/api/CustomerController.java
+++ src/main/java/camp/woowak/lab/web/api/CustomerController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import camp.woowak.lab.customer.service.SignUpCustomerService;
 import camp.woowak.lab.customer.service.command.SignUpCustomerCommand;
 import camp.woowak.lab.web.dto.request.SignUpCustomerRequest;
-import camp.woowak.lab.web.dto.response.ApiResponse;
-import camp.woowak.lab.web.error.ErrorCode;
 import jakarta.validation.Valid;
 
 @RestController
@@ -27,13 +25,9 @@ public class CustomerController {
 		SignUpCustomerCommand command =
 			new SignUpCustomerCommand(request.name(), request.email(), request.password(), request.phone());
 		Long registeredId;
-		try {
-			registeredId = signUpCustomerService.signUp(command);
-		} catch (camp.woowak.lab.customer.exception.InvalidCreationException e) {
-			return ResponseEntity.badRequest().body(ApiResponse.error(ErrorCode.SIGNUP_INVALID_REQUEST));
-		} catch (camp.woowak.lab.customer.exception.DuplicateEmailException e) {
-			return ResponseEntity.ok(ApiResponse.error(ErrorCode.AUTH_DUPLICATE_EMAIL));
-		}
+
+		registeredId = signUpCustomerService.signUp(command);
+
 		return ResponseEntity.created(URI.create("/customers/" + registeredId)).build();
 	}
 }
