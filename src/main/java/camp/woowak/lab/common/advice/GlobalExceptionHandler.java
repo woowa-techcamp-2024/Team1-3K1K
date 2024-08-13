93161c85ddd3c91aa26cee30fd612fc3ae5eb27d
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-13 12:37:45 +0900

    [refactor] GlobalExceptionHandler 응답 방식 수정 - 논의 - #31 에 따라 ResponseEntity 가 아닌 ProblemDetail 를 넘기도록 수정

--- src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
+++ src/main/java/camp/woowak/lab/common/advice/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package camp.woowak.lab.common.advice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,12 +12,11 @@ import lombok.extern.slf4j.Slf4j;
 public class GlobalExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ProblemDetail> handleAllUncaughtException(Exception e) {
+	public ProblemDetail handleAllUncaughtException(Exception e) {
 		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
 			e.getMessage());
 		problemDetail.setProperty("errorCode", "9999");
 
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-			.body(problemDetail);
+		return problemDetail;
 	}
 }
