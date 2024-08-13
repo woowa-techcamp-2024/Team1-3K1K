f941ff9cbe32e131218fcbf0229bb48b479ff48a
june-777 <wlwhswnsrl96@gmail.com>
2024-08-13 14:25:15 +0900

    [feat] 점주 ID 기반으로 매장을 조회하는 시그니쳐 추가
    점주가 가게 메뉴 등록 할 때, 점주에 대한 가게를 조회하는 로직이 필요함

--- src/main/java/camp/woowak/lab/store/repository/StoreRepository.java
+++ src/main/java/camp/woowak/lab/store/repository/StoreRepository.java
@@ -1,8 +1,13 @@
 package camp.woowak.lab.store.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import camp.woowak.lab.store.domain.Store;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
+
+	Optional<Store> findByOwnerId(Long ownerId);
+
 }
