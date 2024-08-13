66284d7389ef5b15c215e2844ee1e03f1e2a57be
june-777 <wlwhswnsrl96@gmail.com>
2024-08-13 12:39:40 +0900

    [feat] 메뉴 도메인 빈 문자열, 공백 문자열 검증 기능

--- src/main/java/camp/woowak/lab/menu/domain/MenuValidator.java
+++ src/main/java/camp/woowak/lab/menu/domain/MenuValidator.java
@@ -10,12 +10,21 @@ public class MenuValidator {
 	public static void validate(final Store store, final MenuCategory menuCategory, final String name,
 								final Integer price, final String imageUrl) {
 		validateNotNull(store, menuCategory, name, price, imageUrl);
+		validateNotBlank(name, imageUrl);
 	}
 
 	private static void validateNotNull(final Object... targets) {
 		for (Object target : targets) {
 			if (target == null) {
 				throw new InvalidMenuCreationException(target + "은 Null 이 될 수 없습니다.");
+			}
+		}
+	}
+
+	private static void validateNotBlank(final String... targets) {
+		for (String target : targets) {
+			if (target.isBlank()) {
+				throw new InvalidMenuCreationException(target + "은 빈 문자열이거나 공백 문자열이 포함될 수 없습니다.");
 			}
 		}
 	}
