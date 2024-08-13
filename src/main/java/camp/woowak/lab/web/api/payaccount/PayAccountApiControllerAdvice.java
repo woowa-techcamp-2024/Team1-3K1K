c04dfc8c74b337ac5ec3cec6a03c7a727d5e6852
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-14 01:32:18 +0900

    [feat] BindingException에 대한 핸들링 추가

--- src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiControllerAdvice.java
+++ src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiControllerAdvice.java
@@ -1,8 +1,13 @@
 package camp.woowak.lab.web.api.payaccount;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import camp.woowak.lab.common.advice.DomainExceptionHandler;
@@ -12,10 +17,23 @@ import camp.woowak.lab.common.exception.NotFoundException;
 import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
 import lombok.extern.slf4j.Slf4j;
 
-//TODO : exception구체화 및 error code 정의
+//TODO : BindingException에 대한 처리
 @DomainExceptionHandler(basePackageClasses = PayAccountApiController.class)
 @Slf4j
 public class PayAccountApiControllerAdvice {
+	@ExceptionHandler(value = MethodArgumentNotValidException.class)
+	public ResponseEntity<?> bindingException(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError)error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		log.warn("Bad Request with parameter binding : {}", errors);
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errors.toString());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+	}
+
 	@ExceptionHandler(value = {BadRequestException.class})
 	public ResponseEntity<ProblemDetail> badRequestException(BadRequestException e) {
 		log.warn("Bad Request", e);
