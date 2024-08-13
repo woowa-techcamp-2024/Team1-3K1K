6c6e338ebfc3b67a1c21e9e59ad715307f685c31
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-13 23:40:45 +0900

    [fix] 세션에 저장된 인증 객체의 CustomerId를 이용해서 포인트를 충전할 수 있도록 수정

--- src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiController.java
+++ src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiController.java
@@ -3,7 +3,6 @@ package camp.woowak.lab.web.api.payaccount;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +12,8 @@ import camp.woowak.lab.payaccount.service.PayAccountChargeService;
 import camp.woowak.lab.payaccount.service.command.PayAccountChargeCommand;
 import camp.woowak.lab.web.api.utils.APIResponse;
 import camp.woowak.lab.web.api.utils.APIUtils;
+import camp.woowak.lab.web.authentication.LoginCustomer;
+import camp.woowak.lab.web.authentication.annotation.AuthenticationPrincipal;
 import camp.woowak.lab.web.dto.request.payaccount.PayAccountChargeRequest;
 import camp.woowak.lab.web.dto.response.payaccount.PayAccountChargeResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -32,16 +33,16 @@ public class PayAccountApiController {
 	 * TODO 2. 인증 방법에 대한 유저 구분값 가져오는 방법 논의
 	 * TODO 3. API Response Format에 대한 논의 후 적용 필요
 	 */
-	@PostMapping("/{payAccountId}/charge")
+	@PostMapping("/charge")
 	public ResponseEntity<APIResponse<PayAccountChargeResponse>> payAccountCharge(
-		@PathVariable("payAccountId") Long payAccountId,
+		@AuthenticationPrincipal LoginCustomer loginCustomer,
 		@Validated @RequestBody PayAccountChargeRequest request) {
-		PayAccountChargeCommand command = new PayAccountChargeCommand(payAccountId, request.amount());
-		log.info("Pay account charge request received. Account ID: {}, Charge Amount: {}", payAccountId,
+		PayAccountChargeCommand command = new PayAccountChargeCommand(loginCustomer.getId(), request.amount());
+		log.info("Pay account charge request received. Account Owner ID: {}, Charge Amount: {}", loginCustomer.getId(),
 			request.amount());
 
 		long remainBalance = payAccountChargeService.chargeAccount(command);
-		log.info("Charge successful. Account ID: {}, New Balance: {}", payAccountId, remainBalance);
+		log.info("Charge successful. Account Owner ID: {}, New Balance: {}", loginCustomer.getId(), remainBalance);
 
 		return APIUtils.of(HttpStatus.OK, new PayAccountChargeResponse(remainBalance));
 	}
