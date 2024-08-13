7e606026b8d61f5ca04504cd33a4c208db86208c
june-777 <wlwhswnsrl96@gmail.com>
2024-08-13 16:27:27 +0900

    [feat] Vendor @Getter 롬복 추가 및 라인 정리

--- src/main/java/camp/woowak/lab/vendor/domain/Vendor.java
+++ src/main/java/camp/woowak/lab/vendor/domain/Vendor.java
@@ -1,13 +1,23 @@
 package camp.woowak.lab.vendor.domain;
 
 import camp.woowak.lab.payaccount.domain.PayAccount;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Vendor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @OneToOne(fetch = FetchType.LAZY)
-    private PayAccount payAccount;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	private PayAccount payAccount;
+
 }
