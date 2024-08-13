be5b1454e158311a20dfdff3a851149b4151c49b
june-777 <wlwhswnsrl96@gmail.com>
2024-08-13 23:54:02 +0900

    [feat] Bean Validation 예외 메시지 구체화

--- src/main/java/camp/woowak/lab/store/service/dto/StoreRegistrationRequest.java
+++ src/main/java/camp/woowak/lab/store/service/dto/StoreRegistrationRequest.java
@@ -7,25 +7,25 @@ import jakarta.validation.constraints.NotNull;
 
 public record StoreRegistrationRequest(
 
-	@NotBlank
+	@NotBlank(message = "가게 이름은 필수값입니다.")
 	String storeName,
 
-	@NotBlank
+	@NotBlank(message = "가게 주소는 필수값입니다.")
 	String storeAddress,
 
-	@NotBlank
+	@NotBlank(message = "가게 번호는 필수값입니다.")
 	String storePhoneNumber,
 
-	@NotBlank
+	@NotBlank(message = "가게 카테고리 이름은 필수값입니다.")
 	String storeCategoryName,
 
-	@NotNull
+	@NotNull(message = "가게 최소 주문 가격은 필수값입니다.")
 	Integer storeMinOrderPrice,
 
-	@NotNull
+	@NotNull(message = "가게 시작 시간은 필수값입니다.")
 	LocalDateTime storeStarTime,
 
-	@NotNull
+	@NotNull(message = "가게 종료 시간은 필수값입니다.")
 	LocalDateTime storeEndTime
 
 ) {
