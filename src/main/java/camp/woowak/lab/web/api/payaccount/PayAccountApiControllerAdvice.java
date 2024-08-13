519c98bdd03d988a828bbc037f93aadd7454ee16
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-14 01:16:09 +0900

    [fix] PayAccount에 대한 Exception을 회의 결과에 맞게 format 수정
    - ProblemDetails를 이용해서 response해주도록 수정

--- src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiControllerAdvice.java
+++ src/main/java/camp/woowak/lab/web/api/payaccount/PayAccountApiControllerAdvice.java
@@ -1,17 +1,14 @@
 package camp.woowak.lab.web.api.payaccount;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import camp.woowak.lab.common.advice.DomainExceptionHandler;
-import camp.woowak.lab.payaccount.exception.DailyLimitExceededException;
-import camp.woowak.lab.payaccount.exception.InsufficientBalanceException;
-import camp.woowak.lab.payaccount.exception.InvalidTransactionAmountException;
+import camp.woowak.lab.common.exception.BadRequestException;
+import camp.woowak.lab.common.exception.ErrorCode;
+import camp.woowak.lab.common.exception.NotFoundException;
 import camp.woowak.lab.payaccount.exception.NotFoundAccountException;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,34 +16,21 @@ import lombok.extern.slf4j.Slf4j;
 @DomainExceptionHandler(basePackageClasses = PayAccountApiController.class)
 @Slf4j
 public class PayAccountApiControllerAdvice {
-	@ExceptionHandler(value = MethodArgumentNotValidException.class)
-	public ResponseEntity<?> bindingException(MethodArgumentNotValidException ex) {
-		Map<String, String> errors = new HashMap<>();
-		ex.getBindingResult().getAllErrors().forEach((error) -> {
-			String fieldName = ((FieldError)error).getField();
-			String errorMessage = error.getDefaultMessage();
-			errors.put(fieldName, errorMessage);
-		});
-		log.warn("Bad Request with parameter binding : {}", errors);
-		return ResponseEntity.badRequest().build();
-	}
-
-	@ExceptionHandler(value = {DailyLimitExceededException.class, InsufficientBalanceException.class,
-		InvalidTransactionAmountException.class})
-	public ResponseEntity<?> badRequestException(Exception e) {
+	@ExceptionHandler(value = {BadRequestException.class})
+	public ResponseEntity<ProblemDetail> badRequestException(BadRequestException e) {
 		log.warn("Bad Request", e);
-		return ResponseEntity.badRequest().build();
+		ErrorCode errorCode = e.errorCode();
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorCode.getMessage());
+		problemDetail.setProperty("errorCode", errorCode.getErrorCode());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
 	}
 
 	@ExceptionHandler(value = {NotFoundAccountException.class})
-	public ResponseEntity<?> notFoundAccountException(Exception e) {
+	public ResponseEntity<ProblemDetail> notFoundAccountException(NotFoundException e) {
 		log.warn("Not Found Request", e);
-		return ResponseEntity.notFound().build();
-	}
-
-	@ExceptionHandler(value = {Exception.class})
-	public ResponseEntity<?> internalServerException(Exception e) {
-		log.error("Internal Server Error", e);
-		return ResponseEntity.internalServerError().build();
+		ErrorCode errorCode = e.errorCode();
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, errorCode.getMessage());
+		problemDetail.setProperty("errorCode", errorCode.getErrorCode());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
 	}
 }
