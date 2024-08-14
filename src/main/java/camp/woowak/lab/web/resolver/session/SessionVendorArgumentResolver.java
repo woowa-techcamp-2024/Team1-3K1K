09e4ff247ca237888ff8c3c78664838b7458ae6f
donghar <donghar@naver.com>
2024-08-14 16:12:08 +0900

    [style] ArgumentResolver 개행
    네이버 컨벤션에 부합하게 개행 처리

--- src/main/java/camp/woowak/lab/web/resolver/session/SessionVendorArgumentResolver.java
+++ src/main/java/camp/woowak/lab/web/resolver/session/SessionVendorArgumentResolver.java
@@ -21,7 +21,7 @@ public class SessionVendorArgumentResolver extends LoginMemberArgumentResolver {
 
 	@Override
 	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-		NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+								  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
 		AuthenticationPrincipal parameterAnnotation = parameter.getParameterAnnotation(AuthenticationPrincipal.class);
 		if (parameterAnnotation == null) {
 			return null;
