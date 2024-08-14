a59089e6d9897a54098e496e7079ad20d700243e
june-777 <wlwhswnsrl96@gmail.com>
2024-08-14 19:10:17 +0900

    [feat] 컨트롤러 공통 응답 처리 - APIResponse 공통 응답
    - HttpStatus를 가져오는 기능
    - body 가 null이면 No content 를 body로 갖는 APIResponse 응답
    - body 가 이미 APIResponse이면 바로 응답
    - HttpStatus와 body로 APIResponse 객체를 응답

--- src/main/java/camp/woowak/lab/web/advice/APIResponseAdvice.java
+++ src/main/java/camp/woowak/lab/web/advice/APIResponseAdvice.java
@@ -1,6 +1,7 @@
 package camp.woowak.lab.web.advice;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +9,13 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import camp.woowak.lab.web.api.utils.APIResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Component
@@ -31,9 +35,27 @@ public class APIResponseAdvice implements ResponseBodyAdvice<Object> {
 								  Class<? extends HttpMessageConverter<?>> selectedConverterType,
 								  ServerHttpRequest request, ServerHttpResponse response
 	) {
-		// TODO
+		HttpStatus status = getHttpStatus(returnType, (ServletServerHttpResponse)response);
+		if (body == null) {
+			return new APIResponse<>(status, "No content");
+		}
 
-		return null;
+		if (body instanceof APIResponse) {
+			return body;
+		}
+
+		return new APIResponse<>(status, body);
 	}
 
+	private HttpStatus getHttpStatus(MethodParameter returnType, ServletServerHttpResponse response) {
+		HttpStatus status;
+		if (returnType.hasMethodAnnotation(ResponseStatus.class)) {
+			ResponseStatus responseStatus = returnType.getMethodAnnotation(ResponseStatus.class);
+			if (responseStatus != null) {
+				return responseStatus.value();
+			}
+		}
+		status = HttpStatus.valueOf(response.getServletResponse().getStatus());
+		return status;
+	}
 }
