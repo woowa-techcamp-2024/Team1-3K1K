1b3fb9dafbeb70769b5fa23de258558c436a4704
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-15 00:59:00 +0900

    [feat] Menu 도메인 엔티티에 필요한 임시 값(메뉴 이름, 가격, 수량) 추가
    - 추후 메뉴 등록에 대한 PR 머지 후 conflict 해결해야함

--- src/main/java/camp/woowak/lab/menu/domain/Menu.java
+++ src/main/java/camp/woowak/lab/menu/domain/Menu.java
@@ -1,18 +1,33 @@
 package camp.woowak.lab.menu.domain;
 
 import camp.woowak.lab.store.domain.Store;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Menu {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Store store;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column
+	private int price;
+
+	@Column
+	private int stock;
 }
