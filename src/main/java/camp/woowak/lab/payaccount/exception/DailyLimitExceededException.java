559e2369e02c4e682a1b39c9d267f2357c0eea13
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-15 00:14:20 +0900

    [feat] CustomException에서 서버 로깅용 메세지를 받는 생성자 추가

--- src/main/java/camp/woowak/lab/payaccount/exception/DailyLimitExceededException.java
+++ src/main/java/camp/woowak/lab/payaccount/exception/DailyLimitExceededException.java
@@ -3,7 +3,7 @@ package camp.woowak.lab.payaccount.exception;
 import camp.woowak.lab.common.exception.BadRequestException;
 
 public class DailyLimitExceededException extends BadRequestException {
-	public DailyLimitExceededException() {
-		super(PayAccountErrorCode.DAILY_LIMIT_EXCEED);
+	public DailyLimitExceededException(String message) {
+		super(PayAccountErrorCode.DAILY_LIMIT_EXCEED, message);
 	}
 }
