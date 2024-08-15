c7300a5d30400282267c7ec7c8dec8665ab86d00
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 21:30:16 +0900

    [fix] CouponIssuanceErrorCode 수정 - errorCode 변경

--- src/main/java/camp/woowak/lab/coupon/exception/CouponIssuanceErrorCode.java
+++ src/main/java/camp/woowak/lab/coupon/exception/CouponIssuanceErrorCode.java
@@ -5,10 +5,10 @@ import org.springframework.http.HttpStatus;
 import camp.woowak.lab.common.exception.ErrorCode;
 
 public enum CouponIssuanceErrorCode implements ErrorCode {
-	INVALID_ISSUANCE(HttpStatus.BAD_REQUEST, "cp_2_1", "잘못된 발급 요청입니다."),
-	NOT_FOUND_COUPON(HttpStatus.NOT_FOUND, "cp_2_2", "존재하지 않는 쿠폰입니다."),
-	EXPIRED_COUPON(HttpStatus.CONFLICT, "cp_2_3", "만료된 쿠폰입니다."),
-	NOT_ENOUGH_COUPON(HttpStatus.CONFLICT, "cp_2_4", "쿠폰이 부족합니다."),
+	INVALID_ISSUANCE(HttpStatus.BAD_REQUEST, "ci_1_1", "잘못된 발급 요청입니다."),
+	NOT_FOUND_COUPON(HttpStatus.NOT_FOUND, "ci_1_2", "존재하지 않는 쿠폰입니다."),
+	EXPIRED_COUPON(HttpStatus.CONFLICT, "ci_1_3", "만료된 쿠폰입니다."),
+	NOT_ENOUGH_COUPON(HttpStatus.CONFLICT, "ci_1_4", "쿠폰이 부족합니다."),
 	;
 
 	int status;
