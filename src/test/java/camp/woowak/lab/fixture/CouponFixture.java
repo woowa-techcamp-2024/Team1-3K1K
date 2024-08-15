e39f161b8616e86fb9170fc74ee2a61177259c5a
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 23:46:34 +0900

    [refactor] CouponFixture 수정 - 명칭 통일 amount -> quantity

--- src/test/java/camp/woowak/lab/fixture/CouponFixture.java
+++ src/test/java/camp/woowak/lab/fixture/CouponFixture.java
@@ -9,7 +9,7 @@ import camp.woowak.lab.coupon.domain.TestCoupon;
  * CouponFixture는 테스트에서 사용할 Coupon 객체를 생성하는 역할을 합니다.
  */
 public interface CouponFixture {
-	default Coupon createCoupon(Long id, String title, int discountAmount, int amount, LocalDateTime expiredAt) {
-		return new TestCoupon(id, title, discountAmount, amount, expiredAt);
+	default Coupon createCoupon(Long id, String title, int discountAmount, int quantity, LocalDateTime expiredAt) {
+		return new TestCoupon(id, title, discountAmount, quantity, expiredAt);
 	}
 }
