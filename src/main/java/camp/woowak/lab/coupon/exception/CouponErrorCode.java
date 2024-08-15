19fae512e41efbae2a839b78fa942e5968b1b8e1
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 23:48:24 +0900

    [feat] CouponErrorCode 수정 - CouponErrorCode.INSUFFICIENT_QUANTITY: 쿠폰 수량 부족 시 에러 코드 구현

--- src/main/java/camp/woowak/lab/coupon/exception/CouponErrorCode.java
+++ src/main/java/camp/woowak/lab/coupon/exception/CouponErrorCode.java
@@ -7,6 +7,7 @@ import camp.woowak.lab.common.exception.ErrorCode;
 public enum CouponErrorCode implements ErrorCode {
 	INVALID_CREATION(HttpStatus.BAD_REQUEST, "cp_1_1", "잘못된 요청입니다."),
 	DUPLICATE_COUPON_TITLE(HttpStatus.CONFLICT, "cp_1_2", "중복된 쿠폰 제목입니다."),
+	INSUFFICIENT_QUANTITY(HttpStatus.CONFLICT, "cp_1_3", "발급 가능한 쿠폰이 부족합니다."),
 	;
 
 	int status;
