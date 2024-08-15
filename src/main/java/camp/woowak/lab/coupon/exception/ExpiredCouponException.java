0edbd0529db00ca637af5155fbd0761f00928de5
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 21:26:59 +0900

    [fix] ExpiredCouponException 수정 - CouponIssuanceErrorCode 를 사용하도록 변경

--- src/main/java/camp/woowak/lab/coupon/exception/ExpiredCouponException.java
+++ src/main/java/camp/woowak/lab/coupon/exception/ExpiredCouponException.java
@@ -4,6 +4,6 @@ import camp.woowak.lab.common.exception.ConflictException;
 
 public class ExpiredCouponException extends ConflictException {
 	public ExpiredCouponException(String message) {
-		super(CouponErrorCode.EXPIRED_COUPON, message);
+		super(CouponIssuanceErrorCode.EXPIRED_COUPON, message);
 	}
 }
