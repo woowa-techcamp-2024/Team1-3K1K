00d2f2a0203e13752bc61b5abcbcff4ed4acda6e
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-15 13:50:34 +0900

    [feat] Cart 저장 API 구현
    - 메뉴 ID를 전달받아 Cart에 저장하는 APIController 구현

---
+++ src/main/java/camp/woowak/lab/web/api/cart/CartApiController.java
@@ -0,0 +1,34 @@
+package camp.woowak.lab.web.api.cart;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import camp.woowak.lab.cart.service.CartService;
+import camp.woowak.lab.cart.service.command.AddCartCommand;
+import camp.woowak.lab.web.authentication.LoginCustomer;
+import camp.woowak.lab.web.authentication.annotation.AuthenticationPrincipal;
+import camp.woowak.lab.web.dto.request.cart.AddCartRequest;
+import camp.woowak.lab.web.dto.response.cart.AddCartResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController("/cart")
+@Slf4j
+public class CartApiController {
+	private final CartService cartService;
+
+	public CartApiController(CartService cartService) {
+		this.cartService = cartService;
+	}
+
+	@PostMapping
+	public AddCartResponse addCart(
+		@AuthenticationPrincipal LoginCustomer loginCustomer,
+		@RequestBody AddCartRequest addCartRequest) {
+		AddCartCommand command = new AddCartCommand(loginCustomer.getId(), addCartRequest.menuId());
+		cartService.addMenu(command);
+
+		log.info("Customer({}) add Menu({}) in Cart", loginCustomer.getId(), addCartRequest.menuId());
+		return new AddCartResponse(true);
+	}
+}
