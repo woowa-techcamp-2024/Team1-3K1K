64db2734212f9226e5469a1572d3f4865c484cce
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 17:44:39 +0900

    [feat] IssueCouponService 수정 - IssueCouponService.issueCoupon ``@Transactional` 추가

--- src/main/java/camp/woowak/lab/coupon/service/IssueCouponService.java
+++ src/main/java/camp/woowak/lab/coupon/service/IssueCouponService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import camp.woowak.lab.coupon.domain.Coupon;
 import camp.woowak.lab.coupon.repository.CouponRepository;
 import camp.woowak.lab.coupon.service.command.IssueCouponCommand;
+import jakarta.transaction.Transactional;
 
 @Service
 public class IssueCouponService {
@@ -18,6 +19,7 @@ public class IssueCouponService {
 	 *
 	 * @throw InvalidCreationCouponException 쿠폰 생성 시 유효하지 않은 값이 입력되었을 경우
 	 */
+	@Transactional
 	public Long issueCoupon(IssueCouponCommand cmd) {
 		Coupon newCoupon = new Coupon(cmd.title(), cmd.discountAmount(), cmd.quantity(), cmd.expiredAt());
 
