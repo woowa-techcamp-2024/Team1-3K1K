b857876d0e9e1a0ab52431ae29cf5b36c609166f
june-777 <wlwhswnsrl96@gmail.com>
2024-08-15 16:22:40 +0900

    [refactor] 서비스 계층 코드 스타일 통일
    1. 서비스 계층 - xxxCommand
    2. 변경함에 따라 서비스 계층에서 Vendor 를 조회하는 로직이 추가됨

--- src/main/java/camp/woowak/lab/store/service/StoreMenuRegistrationService.java
+++ src/main/java/camp/woowak/lab/store/service/StoreMenuRegistrationService.java
@@ -1,8 +1,10 @@
 package camp.woowak.lab.store.service;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import camp.woowak.lab.menu.domain.Menu;
 import camp.woowak.lab.menu.domain.MenuCategory;
@@ -12,8 +14,11 @@ import camp.woowak.lab.menu.repository.MenuRepository;
 import camp.woowak.lab.store.domain.Store;
 import camp.woowak.lab.store.exception.NotFoundStoreException;
 import camp.woowak.lab.store.repository.StoreRepository;
-import camp.woowak.lab.store.service.dto.StoreMenuRegistrationRequest;
+import camp.woowak.lab.store.service.command.MenuLineItem;
+import camp.woowak.lab.store.service.command.StoreMenuRegistrationCommand;
 import camp.woowak.lab.vendor.domain.Vendor;
+import camp.woowak.lab.vendor.exception.NotFoundVendorException;
+import camp.woowak.lab.vendor.repository.VendorRepository;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -27,18 +32,32 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class StoreMenuRegistrationService {
 
+	private final VendorRepository vendorRepository;
 	private final StoreRepository storeRepository;
 	private final MenuRepository menuRepository;
 	private final MenuCategoryRepository menuCategoryRepository;
 
-	public void storeMenuRegistration(final Vendor owner, final StoreMenuRegistrationRequest request) {
-		Store store = findStoreBy(request.storeId());
+	/**
+	 *
+	 * @throws NotFoundMenuCategoryException 가게와 메뉴카테고리 이름으로 메뉴카테고리를 찾지 못할 때 발생
+	 *
+	 */
+	@Transactional
+	public void storeMenuRegistration(final StoreMenuRegistrationCommand command) {
+		Vendor owner = findVendor(command.vendorId());
+
+		Store store = findStoreBy(command.storeId());
 		store.validateOwner(owner);
-		
-		List<StoreMenuRegistrationRequest.MenuLineItem> menuLineItems = request.menuItems();
+
+		List<MenuLineItem> menuLineItems = command.menuItems();
 		List<Menu> menus = createMenus(store, menuLineItems);
 
 		menuRepository.saveAll(menus);
+	}
+
+	private Vendor findVendor(final UUID vendorId) {
+		return vendorRepository.findById(vendorId)
+			.orElseThrow(NotFoundVendorException::new);
 	}
 
 	private Store findStoreBy(final Long storeId) {
@@ -47,14 +66,14 @@ public class StoreMenuRegistrationService {
 	}
 
 	private List<Menu> createMenus(final Store store,
-								   final List<StoreMenuRegistrationRequest.MenuLineItem> menuLineItems
+								   final List<MenuLineItem> menuLineItems
 	) {
 		return menuLineItems.stream()
 			.map(menuLineItem -> createMenu(store, menuLineItem))
 			.toList();
 	}
 
-	private Menu createMenu(final Store store, final StoreMenuRegistrationRequest.MenuLineItem menuLineItem) {
+	private Menu createMenu(final Store store, final MenuLineItem menuLineItem) {
 		MenuCategory menuCategory = findMenuCategoryBy(store, menuLineItem.categoryName());
 		return new Menu(store, menuCategory, menuLineItem.name(), menuLineItem.price(), menuLineItem.imageUrl());
 	}
