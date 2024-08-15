583832c5a849f59592854e216a2cc75e1f5dcf6a
june-777 <wlwhswnsrl96@gmail.com>
2024-08-15 14:56:40 +0900

    [feat] 유효하지 않은 가게 생성시 발생하는 커스텀 예외

---
+++ src/main/java/camp/woowak/lab/store/exception/InvalidStoreCreationException.java
@@ -0,0 +1,16 @@
+package camp.woowak.lab.store.exception;
+
+import camp.woowak.lab.common.exception.BadRequestException;
+import camp.woowak.lab.common.exception.ErrorCode;
+
+public class InvalidStoreCreationException extends BadRequestException {
+
+	public InvalidStoreCreationException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+
+	public InvalidStoreCreationException(ErrorCode errorCode, String message) {
+		super(errorCode, message);
+	}
+	
+}
