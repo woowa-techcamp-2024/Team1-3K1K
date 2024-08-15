7b0a9eba49c7fad1474e87dbc82a381107a90b96
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-15 11:52:37 +0900

    [feat] Cart 저장소의 interface를 in memory로 구현

---
+++ src/main/java/camp/woowak/lab/cart/repository/InMemoryCartRepository.java
@@ -0,0 +1,30 @@
+package camp.woowak.lab.cart.repository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.stereotype.Repository;
+
+import camp.woowak.lab.cart.domain.Cart;
+
+/**
+ * TODO : Null체크에 대한 Validation
+ */
+@Repository
+public class InMemoryCartRepository implements CartRepository {
+	private static final Map<Long, Cart> cartMap = new ConcurrentHashMap<>();
+
+	@Override
+	public Optional<Cart> findByCustomerId(Long customerId) {
+		return Optional.ofNullable(cartMap.get(customerId));
+	}
+
+	@Override
+	public Cart save(Cart cart) {
+		Long customerId = cart.getCustomerId();
+		cartMap.put(customerId, cart);
+
+		return cart;
+	}
+}
