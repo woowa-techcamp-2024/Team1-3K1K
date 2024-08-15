2d0c0172716e807b9c9e6795dbeb9b671e4d12f9
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 21:45:37 +0900

    [fix] TestCoupon 수정 - 만료일 테스트를 위해 수동으로 바꿀 수 있도록 변경

--- src/test/java/camp/woowak/lab/coupon/domain/TestCoupon.java
+++ src/test/java/camp/woowak/lab/coupon/domain/TestCoupon.java
@@ -4,14 +4,25 @@ import java.time.LocalDateTime;
 
 public class TestCoupon extends Coupon {
 	private final Long id;
+	private LocalDateTime expiredAt;
 
 	public TestCoupon(Long id, String title, int discountAmount, int amount, LocalDateTime expiredAt) {
 		super(title, discountAmount, amount, expiredAt);
 		this.id = id;
+		this.expiredAt = expiredAt;
 	}
 
 	@Override
 	public Long getId() {
 		return id;
+	}
+
+	public void setExpiredAt(LocalDateTime expiredAt) {
+		this.expiredAt = expiredAt;
+	}
+
+	@Override
+	public boolean isExpired() {
+		return expiredAt.isBefore(LocalDateTime.now());
 	}
 }
