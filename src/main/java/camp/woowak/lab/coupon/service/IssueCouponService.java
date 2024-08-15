1d47464ab3a6582bb5a9abb6f6db191b00786f51
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 17:04:21 +0900

    [feat] IssueCouponService 구현 - IssueCouponService.issueCoupon: 쿠폰 제목, 할인 금액, 수량, 만료일을 받아 쿠폰을 발급한다.

---
+++ src/main/java/camp/woowak/lab/coupon/service/IssueCouponService.java
@@ -0,0 +1,26 @@
+package camp.woowak.lab.coupon.service;
+
+import org.springframework.stereotype.Service;
+
+import camp.woowak.lab.coupon.domain.Coupon;
+import camp.woowak.lab.coupon.repository.CouponRepository;
+import camp.woowak.lab.coupon.service.command.IssueCouponCommand;
+
+@Service
+public class IssueCouponService {
+	private final CouponRepository couponRepository;
+
+	public IssueCouponService(CouponRepository couponRepository) {
+		this.couponRepository = couponRepository;
+	}
+
+	/**
+	 *
+	 * @throw InvalidCreationCouponException 쿠폰 생성 시 유효하지 않은 값이 입력되었을 경우
+	 */
+	public Long issueCoupon(IssueCouponCommand cmd) {
+		Coupon newCoupon = new Coupon(cmd.title(), cmd.discountAmount(), cmd.quantity(), cmd.expiredAt());
+
+		return couponRepository.save(newCoupon).getId();
+	}
+}
