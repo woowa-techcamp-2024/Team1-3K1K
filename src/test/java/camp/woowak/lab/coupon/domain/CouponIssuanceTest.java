3006357a173aa215a190894674e8887cbe5017aa
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 21:15:28 +0900

    [test]  CouponIssuanceTest 수정 - CouponIssuanceTest.testConstructWithNullCoupon: Coupon 이 Null 인 경우 예외 처리 - CouponIssuanceTest.testConstructWithNullCustomer: Customer 가 Null 인 경우 예외 처리 - CouponIssuanceTest.testConstructWithExpiredCoupon: 만료된 쿠폰인 경우 예외 처리

--- src/test/java/camp/woowak/lab/coupon/domain/CouponIssuanceTest.java
+++ src/test/java/camp/woowak/lab/coupon/domain/CouponIssuanceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import camp.woowak.lab.coupon.exception.CouponExpiredException;
 import camp.woowak.lab.customer.domain.Customer;
 import camp.woowak.lab.fixture.CouponFixture;
 import camp.woowak.lab.fixture.CustomerFixture;
@@ -44,5 +45,44 @@ class CouponIssuanceTest implements CouponFixture, CustomerFixture {
 		assertEquals(customer, couponIssuance.getCustomer());
 		assertNotNull(couponIssuance.getIssuedAt());
 		assertNull(couponIssuance.getUsedAt());
+	}
+
+	@Test
+	@DisplayName("CouponIssuance 생성 테스트 - 쿠폰이 null 인 경우")
+	void testConstructWithNullCoupon() {
+		// given
+		Coupon coupon = null;
+		Customer customer = createCustomer(createPayAccount(), passwordEncoder);
+
+		// when & then
+		assertThrows(NullPointerException.class, () -> new CouponIssuance(coupon, customer));
+	}
+
+	@Test
+	@DisplayName("CouponIssuance 생성 테스트 - 고객이 null 인 경우")
+	void testConstructWithNullCustomer() {
+		// given
+		Coupon coupon = createCoupon(1L, "할인 쿠폰", 1000, 100, LocalDateTime.now().plusDays(7));
+		Customer customer = null;
+
+		// when & then
+		assertThrows(NullPointerException.class, () -> new CouponIssuance(coupon, customer));
+	}
+
+	@Test
+	@DisplayName("CouponIssuance 생성 테스트 - 쿠폰이 만료되었을 때")
+	void testConstructWithExpiredCoupon() {
+		// given
+		Long fakeCouponId = 1L;
+		String title = "할인 쿠폰";
+		int discountAmount = 1000;
+		int quantity = 100;
+		LocalDateTime expiredAt = LocalDateTime.now().minusDays(1);
+		PayAccount payAccount = createPayAccount();
+		Coupon coupon = createCoupon(fakeCouponId, title, discountAmount, quantity, expiredAt);
+		Customer customer = createCustomer(payAccount, passwordEncoder);
+
+		// when & then
+		assertThrows(CouponExpiredException.class, () -> new CouponIssuance(coupon, customer));
 	}
 }
