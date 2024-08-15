8c073f91a05676ea62cff89fac2fd62635d47ea9
donghar <donghar@naver.com>
2024-08-15 13:37:47 +0900

    [fix] 상위 계층 예외의 생성자 변경
    ErrorCode는 클라이언트에게 보낼 응답 코드 및 메시지 포함
    예외를 생성할 때 어떤 문맥에서 발생한 예외인지 추가적인 메시지를 넘겨주게 구현

--- src/main/java/camp/woowak/lab/common/exception/UnauthorizedException.java
+++ src/main/java/camp/woowak/lab/common/exception/UnauthorizedException.java
@@ -1,7 +1,12 @@
 package camp.woowak.lab.common.exception;
 
 public class UnauthorizedException extends HttpStatusException {
+	@Deprecated
 	public UnauthorizedException(ErrorCode errorCode) {
 		super(errorCode);
+	}
+
+	public UnauthorizedException(ErrorCode errorCode, String message) {
+		super(errorCode, message);
 	}
 }
