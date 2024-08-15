c0297316373d275b3228e7ec3d755f0f2e700c79
june-777 <wlwhswnsrl96@gmail.com>
2024-08-15 15:01:28 +0900

    [refactor] 가게 Store 객체를 생성하지 못할 때, InvalidStoreCreationException 를 던지도록 수정

--- src/main/java/camp/woowak/lab/store/domain/StoreValidator.java
+++ src/main/java/camp/woowak/lab/store/domain/StoreValidator.java
@@ -1,10 +1,10 @@
 package camp.woowak.lab.store.domain;
 
-import static camp.woowak.lab.store.exception.StoreException.ErrorCode.*;
+import static camp.woowak.lab.store.exception.StoreErrorCode.*;
 
 import java.time.LocalDateTime;
 
-import camp.woowak.lab.store.exception.StoreException;
+import camp.woowak.lab.store.exception.InvalidStoreCreationException;
 import camp.woowak.lab.vendor.domain.Vendor;
 
 public class StoreValidator {
@@ -31,7 +31,7 @@ public class StoreValidator {
 	private static void validateNotNull(Object... targets) {
 		for (Object target : targets) {
 			if (target == null) {
-				throw new StoreException(NULL_EXIST);
+				throw new InvalidStoreCreationException(NULL_EXIST);
 			}
 		}
 	}
@@ -40,25 +40,28 @@ public class StoreValidator {
 		if (MIN_NAME_LENGTH <= name.length() && name.length() <= MAX_NAME_LENGTH) {
 			return;
 		}
-		throw new StoreException(INVALID_NAME_RANGE);
+		throw new InvalidStoreCreationException(INVALID_NAME_RANGE,
+			"이름의 길이는 " + MIN_NAME_LENGTH + " ~ " + MAX_NAME_LENGTH + " 만 가능합니다. cur: " + name.length());
 	}
 
 	private static void validateAddress(final String address) {
 		if (StoreAddress.DEFAULT_DISTRICT.equals(address)) {
 			return;
 		}
-		throw new StoreException(INVALID_ADDRESS);
+		throw new InvalidStoreCreationException(INVALID_ADDRESS, "주소는 송파만 가능합니다. cur: " + address);
 	}
 
 	private static void validateMinOrderPrice(final Integer minOrderPrice) {
 		if (minOrderPrice < MIN_ORDER_PRICE) {
-			throw new StoreException(INVALID_MIN_ORDER_PRICE);
+			throw new InvalidStoreCreationException(INVALID_MIN_ORDER_PRICE,
+				"최소 주문 금액은 " + MIN_ORDER_PRICE + " 보다 이상이어야 합니다. cur: " + minOrderPrice);
 		}
 	}
 
 	private static void validateUnitOrderPrice(final Integer minOrderPrice) {
 		if (minOrderPrice % UNIT_OF_MIN_ORDER_PRICE != 0) {
-			throw new StoreException(INVALID_UNIT_OF_MIN_ORDER_PRICE);
+			throw new InvalidStoreCreationException(INVALID_UNIT_OF_MIN_ORDER_PRICE,
+				"최소 주문 금액의 단위는 천원이어야 합니다. cur: " + minOrderPrice);
 		}
 	}
 
@@ -66,19 +69,20 @@ public class StoreValidator {
 									 final LocalDateTime endTime
 	) {
 		if (isInvalidStoreTimeUnit(startTime)) {
-			throw new StoreException(INVALID_TIME_UNIT);
+			throw new InvalidStoreCreationException(INVALID_TIME_UNIT, "가게 시간은 분단위까지 가능합니다. cur: " + startTime);
 		}
 
 		if (isInvalidStoreTimeUnit(endTime)) {
-			throw new StoreException(INVALID_TIME_UNIT);
+			throw new InvalidStoreCreationException(INVALID_TIME_UNIT, "가게 시간은 분단위까지 가능합니다. cur:" + endTime);
 		}
 
 		if (endTime.isBefore(startTime)) {
-			throw new StoreException(INVALID_TIME);
+			throw new InvalidStoreCreationException(INVALID_TIME,
+				"가게 종료 시간이 시작 시간보다 이전입니다. cur:" + startTime + "," + endTime);
 		}
 
 		if (startTime.isEqual(endTime)) {
-			throw new StoreException(INVALID_TIME);
+			throw new InvalidStoreCreationException(INVALID_TIME, "가게 시작 시간과 종료 시간이 같습니다. cur:" + startTime + endTime);
 		}
 	}
 
