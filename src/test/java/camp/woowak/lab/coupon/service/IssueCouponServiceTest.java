08ac7d017155f7bdda0067402a0b23b4e1f1233e
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 17:01:07 +0900

    [fix] IssueCouponServiceTest 수정 - Coupon field 추가에 따른 변경 사항

--- src/test/java/camp/woowak/lab/coupon/service/IssueCouponServiceTest.java
+++ src/test/java/camp/woowak/lab/coupon/service/IssueCouponServiceTest.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import camp.woowak.lab.coupon.domain.Coupon;
 import camp.woowak.lab.coupon.repository.CouponRepository;
+import camp.woowak.lab.coupon.service.command.IssueCouponCommand;
 import camp.woowak.lab.fixture.CouponFixture;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,23 +22,28 @@ class IssueCouponServiceTest implements CouponFixture {
 	@InjectMocks
 	private IssueCouponService issueCouponService;
 
-	@MockBean
+	@Mock
 	private CouponRepository couponRepository;
 
 	@Test
 	@DisplayName("쿠폰 생성 테스트 - 쿠폰은 고정 할인 금액, 개수, 만료일을 입력해 할인 쿠폰을 등록할 수 있다.")
 	void testIssueCoupon() {
 		// given
+		Long fakeId = 1L;
+		String title = "테스트 쿠폰";
 		int discountAmount = 1000;
 		int quantity = 100;
 		LocalDateTime expiredAt = LocalDateTime.now().plusDays(7);
-		Coupon coupon = createCoupon(discountAmount, quantity, expiredAt);
-		IssueCouponCommand cmd = new IssueCouponCommand(discountAmount, quantity, expiredAt);
-		given(couponRepository.save(cmd)).willReturn(coupon);
+		Coupon coupon = createCoupon(fakeId, title, discountAmount, quantity, expiredAt);
+		IssueCouponCommand cmd = new IssueCouponCommand(title, discountAmount, quantity, expiredAt);
+		given(couponRepository.save(any())).willReturn(coupon);
+
 		// when
-		Long id = issueCouponService.issueCoupon(cmd);
+		issueCouponService.issueCoupon(cmd);
 
 		// then
+		assertEquals(fakeId, coupon.getId());
+		assertEquals(title, coupon.getTitle());
 		assertEquals(discountAmount, coupon.getDiscountAmount());
 		assertEquals(quantity, coupon.getQuantity());
 		assertEquals(expiredAt, coupon.getExpiredAt());
