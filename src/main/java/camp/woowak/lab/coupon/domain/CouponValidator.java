3b7efe153bd99e3ed5932b3d4554ed5f5de6118b
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-15 16:08:25 +0900

    [feat] CouponValidator 추가 - 쿠폰의 수량, 할인 금액, 만료일을 검증한다.

---
+++ src/main/java/camp/woowak/lab/coupon/domain/CouponValidator.java
@@ -0,0 +1,38 @@
+package camp.woowak.lab.coupon.domain;
+
+import java.time.LocalDateTime;
+
+import camp.woowak.lab.coupon.exception.InvalidCreationCouponException;
+
+public class CouponValidator {
+	private static final int MIN_DISCOUNT_AMOUNT = 0;
+	private static final int MIN_QUANTITY = 0;
+
+	private CouponValidator() {
+	}
+
+	public static void validate(int discountAmount, int quantity, LocalDateTime expiredAt) {
+		validateDiscountAmount(discountAmount);
+		validateQuantity(quantity);
+		validateExpiredDate(expiredAt);
+	}
+
+	private static void validateDiscountAmount(int discountAmount) {
+		if (discountAmount < MIN_DISCOUNT_AMOUNT) {
+			throw new InvalidCreationCouponException(
+				discountAmount + "should be greater than or equal to " + MIN_DISCOUNT_AMOUNT);
+		}
+	}
+
+	private static void validateQuantity(int quantity) {
+		if (quantity < MIN_QUANTITY) {
+			throw new InvalidCreationCouponException(quantity + "should be greater than " + MIN_QUANTITY);
+		}
+	}
+
+	private static void validateExpiredDate(LocalDateTime expiredAt) {
+		if (expiredAt.isBefore(LocalDateTime.now())) {
+			throw new InvalidCreationCouponException("expiredAt should be after now");
+		}
+	}
+}
