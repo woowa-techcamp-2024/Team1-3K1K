54f0b0c442f0f843a1e543b9df66b2ab5d415940
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-16 09:28:28 +0900

    [chore] 매장의 리스트를 조회해오는 메서드에 TODO 작성

--- src/main/java/camp/woowak/lab/web/dao/store/StoreDao.java
+++ src/main/java/camp/woowak/lab/web/dao/store/StoreDao.java
@@ -10,17 +10,23 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import camp.woowak.lab.store.domain.QStore;
 import camp.woowak.lab.store.domain.Store;
 import camp.woowak.lab.web.dto.response.store.StoreInfoResponse;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 
 @Repository
 @Transactional(readOnly = true)
 public class StoreDao {
 	private final JPAQueryFactory qf;
+
 	public StoreDao(JPAQueryFactory qf) {
 		this.qf = qf;
 	}
 
+	/**
+	 * TODO 페이지별 매장 리스트를 조회할 수 있다.
+	 * TODO 주문이 많은 순으로 정렬할 수 있다.
+	 * TODO 최소 주문 가격순으로 정렬할 수 있다.
+	 * TODO 최소 주문 가격으로 필터링할 수 있다.
+	 * TODO 매장의 카테고리로 필터링할 수 잇다.
+	 */
 	public StoreInfoResponse findAllStoreList() {
 		QStore store = QStore.store;
 
