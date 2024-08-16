cb113bd7efc6613b78e6314b9b69856f07f70047
donghar <donghar@naver.com>
2024-08-16 21:43:28 +0900

    [feat] 주문 생성 서비스
    주문은 1)같은 가게의 메뉴에 대해 2)재고가 있고 3)결제할 포인트가 충분할 때 진행할 수 있다.

--- src/main/java/camp/woowak/lab/order/service/OrderCreationService.java
+++ src/main/java/camp/woowak/lab/order/service/OrderCreationService.java
@@ -8,41 +8,55 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import camp.woowak.lab.cart.domain.Cart;
+import camp.woowak.lab.cart.domain.vo.CartItem;
 import camp.woowak.lab.cart.repository.CartRepository;
 import camp.woowak.lab.customer.domain.Customer;
 import camp.woowak.lab.customer.repository.CustomerRepository;
-import camp.woowak.lab.order.domain.CompositeOrderValidator;
 import camp.woowak.lab.order.domain.Order;
-import camp.woowak.lab.order.domain.OrderValidator;
+import camp.woowak.lab.order.domain.SingleStoreOrderValidator;
+import camp.woowak.lab.order.domain.StockRequester;
 import camp.woowak.lab.order.exception.EmptyCartException;
 import camp.woowak.lab.order.repository.OrderRepository;
 import camp.woowak.lab.order.service.command.OrderCreationCommand;
+import camp.woowak.lab.store.domain.Store;
+import camp.woowak.lab.store.exception.NotFoundStoreException;
+import camp.woowak.lab.store.repository.StoreRepository;
 
 @Service
 @Transactional
 public class OrderCreationService {
 	private final OrderRepository orderRepository;
 	private final CartRepository cartRepository;
+	private final StoreRepository storeRepository;
 	private final CustomerRepository customerRepository;
-	private final CompositeOrderValidator orderValidators;
+	private final SingleStoreOrderValidator singleStoreOrderValidator;
+	private final StockRequester stockRequester;
 
 	public OrderCreationService(OrderRepository orderRepository, CartRepository cartRepository,
-								CustomerRepository customerRepository, List<OrderValidator> orderValidators) {
+								StoreRepository storeRepository, CustomerRepository customerRepository,
+								SingleStoreOrderValidator singleStoreOrderValidator, StockRequester stockRequester) {
 		this.orderRepository = orderRepository;
 		this.cartRepository = cartRepository;
+		this.storeRepository = storeRepository;
 		this.customerRepository = customerRepository;
-		this.orderValidators = new CompositeOrderValidator(orderValidators);
+		this.singleStoreOrderValidator = singleStoreOrderValidator;
+		this.stockRequester = stockRequester;
 	}
 
 	public Long create(OrderCreationCommand cmd) {
 		UUID requesterId = cmd.requesterId();
 		Customer requester = customerRepository.findByIdOrThrow(requesterId);
 		Optional<Cart> findCart = cartRepository.findByCustomerId(requesterId.toString());
-		Cart cart;
-		if (findCart.isEmpty() || (cart = findCart.get()).getMenuList().isEmpty()) {
+		List<CartItem> cartItems;
+		if (findCart.isEmpty() || (cartItems = findCart.get().getCartItems()) == null || cartItems.isEmpty()) {
 			throw new EmptyCartException("구매자 " + cmd.requesterId() + "가 비어있는 카트로 주문을 시도했습니다.");
 		}
-		Order savedOrder = orderRepository.save(new Order(requester, cart.getMenuList(), orderValidators));
+		Optional<Store> findStore = storeRepository.findById(cartItems.get(0).getStoreId());
+		if (findStore.isEmpty()) {
+			throw new NotFoundStoreException("등록되지 않은 가게의 상품을 주문했습니다.");
+		}
+		Order savedOrder = orderRepository.save(
+			new Order(requester, findStore.get(), cartItems, singleStoreOrderValidator, stockRequester));
 		return savedOrder.getId();
 	}
 }
