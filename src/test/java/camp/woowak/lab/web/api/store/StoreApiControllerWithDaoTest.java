ca19dc4c663f192b6292ded566cbb26280796740
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-16 16:18:27 +0900

    [test] StoreApiController에서 기존 리스트를 전부 불러오는 로직에서 페이징 처리된 리스트를 반환하는 테스트코드 작성 및 StoreDummiesFixture 적용

--- src/test/java/camp/woowak/lab/web/api/store/StoreApiControllerWithDaoTest.java
+++ src/test/java/camp/woowak/lab/web/api/store/StoreApiControllerWithDaoTest.java
@@ -5,13 +5,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.io.UnsupportedEncodingException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,16 +22,14 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import camp.woowak.lab.payaccount.domain.PayAccount;
 import camp.woowak.lab.payaccount.repository.PayAccountRepository;
 import camp.woowak.lab.store.domain.Store;
-import camp.woowak.lab.store.domain.StoreAddress;
-import camp.woowak.lab.store.domain.StoreCategory;
 import camp.woowak.lab.store.repository.StoreCategoryRepository;
 import camp.woowak.lab.store.repository.StoreRepository;
-import camp.woowak.lab.vendor.domain.Vendor;
 import camp.woowak.lab.vendor.repository.VendorRepository;
-import camp.woowak.lab.web.authentication.NoOpPasswordEncoder;
+import camp.woowak.lab.web.dao.store.StoreDummiesFixture;
+import camp.woowak.lab.web.dto.request.store.StoreInfoListRequest;
+import camp.woowak.lab.web.dto.request.store.StoreInfoListRequestConst;
 import camp.woowak.lab.web.dto.response.store.StoreInfoListResponse;
 import jakarta.transaction.Transactional;
 
@@ -43,92 +37,85 @@ import jakarta.transaction.Transactional;
 @AutoConfigureMockMvc
 @DisplayName("StoreApiController 클래스 With Dao")
 @Transactional
-public class StoreApiControllerWithDaoTest {
-	@Autowired
-	private PayAccountRepository payAccountRepository;
-	@Autowired
-	private StoreRepository storeRepository;
-	@Autowired
-	private StoreCategoryRepository storeCategoryRepository;
-	@Autowired
-	private VendorRepository vendorRepository;
-	@Autowired
-	private ObjectMapper objectMapper;
-	@Autowired
-	private MockMvc mvc;
+public class StoreApiControllerWithDaoTest extends StoreDummiesFixture {
+	private final ObjectMapper objectMapper;
+	private final MockMvc mvc;
+	private static List<Store> dummies;
+	private final int dummyCount;
+	private final int size = StoreInfoListRequest.DEFAULT_PAGE_SIZE;
 
-	private List<Store> stores;
+	@Autowired
+	public StoreApiControllerWithDaoTest(PayAccountRepository payAccountRepository, StoreRepository storeRepository,
+										 StoreCategoryRepository storeCategoryRepository,
+										 VendorRepository vendorRepository,
+										 ObjectMapper objectMapper, MockMvc mvc) {
+		super(storeRepository, storeCategoryRepository, vendorRepository, payAccountRepository);
+		this.objectMapper = objectMapper;
+		this.mvc = mvc;
+		this.dummyCount = 105;
+	}
+
+	@BeforeEach
+	public void setUpDummies() {
+		dummies = createDummyStores(dummyCount);
+	}
 
 	@Nested
 	@DisplayName("getStoreInfos메서드: GET /stores")
 	class GetStoresInfo {
 		private final String BASE_URL = "/stores";
 
-		@BeforeEach
-		void setUpDummyStores() throws Exception {
-			stores = createDummyStores(10);
-		}
-
 		@Test
-		@DisplayName("아무런 파라미터가 존재하지 않으면 id기준 오름차순으로 모든 store를 보여준다.")
-		void getAllStoresInfoSuccess() throws Exception {
+		@DisplayName("Default 사이즈 만큼 요청한 페이지의 컨텐츠가 나온다. [컨텐츠가 가득 찬 경우]")
+		void testWithPageRequestOrderByIdAsc() throws Exception {
 			//given
+			int page = 0;
 
 			//when & then
-			ResultActions actions = mvc.perform(get(BASE_URL))
+			ResultActions actions = mvc.perform(get(getBaseURL(page)))
 				.andExpect(status().isOk());
 
 			//기본적으로는 ID 오름차순
-			assertResults(actions, stores, (o1, o2) -> Long.compare(o1.getId(), o2.getId()));
+			int offset = page * size;
+			assertResults(actions, dummies, (o1, o2) -> Long.compare(o1.getId(), o2.getId()), offset, size);
 		}
 
-		void assertResults(ResultActions actions, List<Store> stores, Comparator<Store> comparator) throws
-			UnsupportedEncodingException, JsonProcessingException {
-			stores.sort(comparator);
-			String responseString = actions.andReturn()
-				.getResponse().getContentAsString();
-			Map<String, Object> responseData = objectMapper.readValue(responseString, Map.class);
-			String actualResponseDataString = objectMapper.writeValueAsString(responseData.get("data"));
+		@Test
+		@DisplayName("Default 사이즈 만큼 요청한 페이지의 컨텐츠가 나온다. [페이지가 모두 차지 않은 경우]")
+		void testWithPageRequestOrderByIdAsc_Not_Enough_Stores() throws Exception {
+			//given
+			int page = 5;
 
-			StoreInfoListResponse expectedResponse = StoreInfoListResponse.of(stores);
-			String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
+			//when & then
+			ResultActions actions = mvc.perform(get(getBaseURL(page)))
+				.andExpect(status().isOk());
 
-			assertThat(actualResponseDataString).isEqualTo(expectedResponseString);
+			//기본적으로는 ID 오름차순
+			int offset = page * size;
+			assertResults(actions, dummies, (o1, o2) -> Long.compare(o1.getId(), o2.getId()), offset, size);
 		}
 
-	}
-
-	private List<Store> createDummyStores(int numberOfStores) {
-		List<Store> stores = new ArrayList<>();
-
-		Vendor vendor = createDummyVendor();
-		for (int i = 0; i < numberOfStores; i++) {
-			StoreCategory storeCategory = createRandomDummyStoreCategory();
-			String name = "Store " + (i + 1);
-			String address = StoreAddress.DEFAULT_DISTRICT;
-			String phoneNumber = "123-456-789" + (i % 10);
-			Integer minOrderPrice = 5000 + (new Random().nextInt(10000)) / 1000 * 1000;
-			LocalDateTime startTime = LocalDateTime.now().plusHours(new Random().nextInt(10)).withSecond(0).withNano(0);
-			LocalDateTime endTime = startTime.plusHours(new Random().nextInt(20) + 1);
-
-			Store store = new Store(vendor, storeCategory, name, address, phoneNumber, minOrderPrice, startTime,
-				endTime);
-			stores.add(store);
+		private String getBaseURL(int page) {
+			return BASE_URL + "?" + StoreInfoListRequestConst.PAGE_KEY + "=" + page;
 		}
-
-		storeRepository.saveAllAndFlush(stores);
-		return stores;
 	}
 
-	private Vendor createDummyVendor() {
-		PayAccount payAccount = new PayAccount();
-		payAccountRepository.saveAndFlush(payAccount);
-		return vendorRepository.saveAndFlush(
-			new Vendor("VendorName", "email@gmail.com", "Password123!", "010-1234-5678",
-				payAccount, new NoOpPasswordEncoder()));
-	}
+	private void assertResults(ResultActions actions, List<Store> stores, Comparator<Store> comparator,
+							   int offset, int size) throws
+		UnsupportedEncodingException, JsonProcessingException {
+		String responseString = actions.andReturn()
+			.getResponse().getContentAsString();
+		Map<String, Object> responseData = objectMapper.readValue(responseString, Map.class);
+		String actualResponseDataString = objectMapper.writeValueAsString(responseData.get("data"));
 
-	private StoreCategory createRandomDummyStoreCategory() {
-		return storeCategoryRepository.saveAndFlush(new StoreCategory(UUID.randomUUID().toString()));
+		List<Store> expectedList = stores.stream()
+			.sorted(comparator)
+			.skip(offset)
+			.limit(size)
+			.toList();
+		StoreInfoListResponse expectedResponse = StoreInfoListResponse.of(expectedList);
+		String expectedResponseString = objectMapper.writeValueAsString(expectedResponse);
+
+		assertThat(actualResponseDataString).isEqualTo(expectedResponseString);
 	}
 }
