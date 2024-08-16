2b99e662d570f17a31d1a50e5ac3bd9587995115
donghar <donghar@naver.com>
2024-08-16 21:38:24 +0900

    [feat] Menu 검증 서비스
    주문은 같은 가게의 메뉴만 주문할 수 있다.

--- src/main/java/camp/woowak/lab/order/domain/SingleStoreOrderValidator.java
+++ src/main/java/camp/woowak/lab/order/domain/SingleStoreOrderValidator.java
@@ -4,21 +4,19 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import camp.woowak.lab.customer.domain.Customer;
-import camp.woowak.lab.menu.domain.Menu;
+import camp.woowak.lab.cart.domain.vo.CartItem;
 import camp.woowak.lab.order.exception.EmptyCartException;
 import camp.woowak.lab.order.exception.MultiStoreOrderException;
+import camp.woowak.lab.store.domain.Store;
 
 @Component
-public class SingleStoreOrderValidator implements OrderValidator {
-	@Override
-	public void check(Customer requester, List<Menu> orderedMenus) {
-		if (orderedMenus == null || orderedMenus.isEmpty()) {
+public class SingleStoreOrderValidator {
+	public void check(Store store, List<CartItem> cartItems) {
+		if (cartItems == null || cartItems.isEmpty()) {
 			throw new EmptyCartException("최소 하나 이상의 메뉴를 주문해야 합니다.");
 		}
-		Long storeId = orderedMenus.get(0).getStore().getId();
-		for (Menu orderedMenu : orderedMenus) {
-			if (!orderedMenu.getStore().getId().equals(storeId)) {
+		for (CartItem cartItem : cartItems) {
+			if (!cartItem.getStoreId().equals(store.getId())) {
 				throw new MultiStoreOrderException("다른 가게의 메뉴를 같이 주문할 수 없습니다.");
 			}
 		}
