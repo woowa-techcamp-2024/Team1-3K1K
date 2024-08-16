6d5b039555e357e4e7525ccc47437b079f9297cc
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-16 16:16:06 +0900

    [refactor] StoreDao에 StoreDummiesFixture 적용 및 기존 리스트 검증 방식을 페이지 검증 방식으로 수정

--- src/test/java/camp/woowak/lab/web/dao/store/StoreDaoTest.java
+++ src/test/java/camp/woowak/lab/web/dao/store/StoreDaoTest.java
@@ -2,7 +2,8 @@ package camp.woowak.lab.web.dao.store;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,72 +13,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import camp.woowak.lab.fixture.VendorFixture;
-import camp.woowak.lab.payaccount.domain.PayAccount;
 import camp.woowak.lab.payaccount.repository.PayAccountRepository;
 import camp.woowak.lab.store.domain.Store;
-import camp.woowak.lab.store.domain.StoreCategory;
 import camp.woowak.lab.store.repository.StoreCategoryRepository;
 import camp.woowak.lab.store.repository.StoreRepository;
-import camp.woowak.lab.vendor.domain.Vendor;
 import camp.woowak.lab.vendor.repository.VendorRepository;
-import camp.woowak.lab.web.authentication.NoOpPasswordEncoder;
+import camp.woowak.lab.web.dto.request.store.StoreInfoListRequest;
 import camp.woowak.lab.web.dto.response.store.StoreInfoListResponse;
 
 @SpringBootTest
 @Transactional
 @DisplayName("StoreDao 클래스")
-class StoreDaoTest implements VendorFixture {
-	@Autowired
-	private StoreRepository storeRepository;
-	@Autowired
-	private StoreCategoryRepository storeCategoryRepository;
-	@Autowired
-	private VendorRepository vendorRepository;
-	@Autowired
-	private PayAccountRepository payAccountRepository;
-	@Autowired
-	private StoreDao storeDao;
+class StoreDaoTest extends StoreDummiesFixture {
+	private final StoreDao storeDao;
+	private final int dummyCount;
+	private List<Store> dummies;
 
-	private Store store1;
-	private Store store2;
+	@Autowired
+	public StoreDaoTest(StoreDao storeDao, StoreRepository storeRepository,
+						StoreCategoryRepository storeCategoryRepository,
+						VendorRepository vendorRepository, PayAccountRepository payAccountRepository) {
+		super(storeRepository, storeCategoryRepository, vendorRepository, payAccountRepository);
+		this.storeDao = storeDao;
+		this.dummyCount = 105;
+	}
 
 	@BeforeEach
-	void setUp() {
-		PayAccount payAccount = new PayAccount();
-		payAccountRepository.save(payAccount);
-		Vendor vendor = createVendor(payAccount, new NoOpPasswordEncoder());
-		vendorRepository.saveAndFlush(vendor);
-		StoreCategory storeCategory = new StoreCategory("CategoryName");
-		storeCategoryRepository.saveAndFlush(storeCategory);
-
-		store1 = new Store(vendor, storeCategory, "Store1", "송파", "123-456-7890", 6000,
-			LocalDateTime.now().minusHours(1).withSecond(0).withNano(0),
-			LocalDateTime.now().plusHours(1).withSecond(0).withNano(0));
-		store2 = new Store(vendor, storeCategory, "Store2", "송파", "987-654-3210", 7000,
-			LocalDateTime.now().minusHours(1).withSecond(0).withNano(0),
-			LocalDateTime.now().minusMinutes(1).withSecond(0).withNano(0));
-		storeRepository.saveAndFlush(store1);
-		storeRepository.saveAndFlush(store2);
+	void setUpDummies() {
+		this.dummies = createDummyStores(dummyCount);
 	}
 
 	@Nested
 	@DisplayName("findAllStoreList 메서드는")
 	class FindAllStoreList {
+		private final int size = StoreInfoListRequest.DEFAULT_PAGE_SIZE;
 
 		@Test
-		@DisplayName("아무런 파라미터값이 없으면 id값을 기준으로 오름차순 정렬되어 보내진다.")
-		void testWithNoneArgumentOrderByIdAsc() {
-			StoreInfoListResponse response = storeDao.findAllStoreList();
+		@DisplayName("Default 사이즈 만큼 요청한 페이지의 컨텐츠가 나온다.(페이지가 모두 찬 경우)")
+		void testWithPageRequestOrderByIdAsc() {
+			//given
+			int page = 1;
+			StoreInfoListRequest request = new StoreInfoListRequest(page);
 
+			//when
+			StoreInfoListResponse response = storeDao.findAllStoreList(request);
+
+			//then
 			assertThat(response).isNotNull();
-			assertThat(response.getStores()).size().isEqualTo(2);
 
-			StoreInfoListResponse.InfoResponse info1 = response.getStores().get(0);
-			assertStoresInfo(info1, store1);
+			//기대되는 결과값을 만들기 위한 조건
+			Comparator<Store> comparator = (o1, o2) -> Long.compare(o1.getId(), o2.getId());
+			int offset = page * size;
+			assertResponse(response, comparator, offset, size);
+		}
 
-			StoreInfoListResponse.InfoResponse info2 = response.getStores().get(1);
-			assertStoresInfo(info2, store2);
+		@Test
+		@DisplayName("Default 사이즈 만큼 요청한 페이지의 컨텐츠가 나온다. (페이지가 모두 차지 않은 경우)")
+		void testWithPageRequestOrderByIdAsc_Not_Enough_Stores() {
+			//given
+			int page = 5;
+			StoreInfoListRequest request = new StoreInfoListRequest(page);
+
+			//when
+			StoreInfoListResponse response = storeDao.findAllStoreList(request);
+
+			//then
+			assertThat(response).isNotNull();
+
+			//기대되는 결과값을 만들기 위한 조건
+			Comparator<Store> comparator = (o1, o2) -> Long.compare(o1.getId(), o2.getId());
+			int offset = page * size;
+			assertResponse(response, comparator, offset, size);
+		}
+	}
+
+	private void assertResponse(StoreInfoListResponse response, Comparator<Store> comparator, int offset, int size) {
+		List<StoreInfoListResponse.InfoResponse> stores = response.getStores();
+
+		List<Store> expectedList = dummies.stream()
+			.sorted(comparator)
+			.skip(offset)
+			.limit(size)
+			.toList();
+
+		assertThat(stores.size()).isEqualTo(expectedList.size());
+		for (int i = 0; i < stores.size(); i++) {
+			StoreInfoListResponse.InfoResponse actual = stores.get(i);
+			System.out.println("actual.getStoreId() = " + actual.getStoreId());
+			Store expected = expectedList.get(i);
+			assertStoresInfo(actual, expected);
 		}
 	}
 
