ee0505ef06cddca5fb1c9eb6f95236723a3c0ede
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-17 16:46:10 +0900

    [feat] 가게 정보 리스트를 받을 때 추가 조건들에 대한 key값 및 DEFAULT 상수들을 정의
    - 값을 언제든 바꿀 수 있음

---
+++ src/main/java/camp/woowak/lab/web/dto/request/store/StoreInfoListRequestConst.java
@@ -0,0 +1,16 @@
+package camp.woowak.lab.web.dto.request.store;
+
+public final class StoreInfoListRequestConst {
+	public static final String PAGE_KEY = "page";
+	public static final String SORT_BY_KEY = "sortBy";
+	public static final String ORDER_KEY = "order";
+	public static final String FILTER_BY_KEY = "filterBy";
+	public static final String FILTER_VALUE_KEY = "filterValue";
+
+	public static final int DEFAULT_PAGE_SIZE = 20;
+	public static final int DEFAULT_PAGE_NUMBER = 0;
+	public static final int DEFAULT_ORDER = 0;
+
+	private StoreInfoListRequestConst() {
+	}
+}
