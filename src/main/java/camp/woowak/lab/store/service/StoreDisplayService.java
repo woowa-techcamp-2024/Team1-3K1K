16c6667bdab9d747ae7a925603b416f4aee1fe5b
june-777 <wlwhswnsrl96@gmail.com>
2024-08-17 19:27:10 +0900

    [feat] 가게 전시 응답에 가게 메뉴를 포함

--- src/main/java/camp/woowak/lab/store/service/StoreDisplayService.java
+++ src/main/java/camp/woowak/lab/store/service/StoreDisplayService.java
@@ -1,8 +1,12 @@
 package camp.woowak.lab.store.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import camp.woowak.lab.menu.domain.Menu;
+import camp.woowak.lab.menu.repository.MenuRepository;
 import camp.woowak.lab.store.domain.Store;
 import camp.woowak.lab.store.exception.NotFoundStoreException;
 import camp.woowak.lab.store.repository.StoreRepository;
@@ -14,16 +18,29 @@ import lombok.RequiredArgsConstructor;
 public class StoreDisplayService {
 
 	private final StoreRepository storeRepository;
+	private final MenuRepository menuRepository;
 
 	@Transactional(readOnly = true)
 	public StoreDisplayResponse findStore(final Long storeId) {
 		Store store = findStoreById(storeId);
-		return StoreDisplayResponse.of(store);
+		List<Menu> storeMenus = findMenusByStore(store.getId());
+
+		return StoreDisplayResponse.of(store, mapFrom(storeMenus));
 	}
 
 	private Store findStoreById(final Long storeId) {
 		return storeRepository.findById(storeId)
 			.orElseThrow(() -> new NotFoundStoreException(storeId + "의 가게를 찾을 수 없습니다."));
+	}
+
+	private List<Menu> findMenusByStore(final Long storeId) {
+		return menuRepository.findByStoreId(storeId);
+	}
+
+	private List<StoreDisplayResponse.MenuDisplayResponse> mapFrom(final List<Menu> menus) {
+		return menus.stream()
+			.map(StoreDisplayResponse.MenuDisplayResponse::of)
+			.toList();
 	}
 
 }
--- src/main/java/camp/woowak/lab/store/service/response/StoreDisplayResponse.java
+++ src/main/java/camp/woowak/lab/store/service/response/StoreDisplayResponse.java
@@ -1,8 +1,10 @@
 package camp.woowak.lab.store.service.response;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.UUID;
 
+import camp.woowak.lab.menu.domain.Menu;
 import camp.woowak.lab.store.domain.Store;
 
 public record StoreDisplayResponse(
@@ -20,11 +22,12 @@ public record StoreDisplayResponse(
 	LocalTime storeEndTime,
 
 	UUID vendorId,
-	String vendorName
+	String vendorName,
 
+	List<MenuDisplayResponse> menus
 ) {
 
-	public static StoreDisplayResponse of(final Store store) {
+	public static StoreDisplayResponse of(final Store store, final List<MenuDisplayResponse> menus) {
 		return new StoreDisplayResponse(
 			store.getId(),
 			store.getName(),
@@ -39,7 +42,29 @@ public record StoreDisplayResponse(
 			store.getStoreEndTime(),
 
 			store.getVendorId(),
-			store.getVendorName()
+			store.getVendorName(),
+			menus
 		);
+	}
+
+	public record MenuDisplayResponse(
+		Long menuCategoryId,
+		String menuCategoryName,
+
+		Long menuId,
+		String menuName,
+		Integer menuPrice
+	) {
+
+		public static MenuDisplayResponse of(final Menu menu) {
+			return new MenuDisplayResponse(
+				menu.getMenuCategoryId(),
+				menu.getMenuCategoryName(),
+
+				menu.getId(),
+				menu.getName(),
+				menu.getPrice()
+			);
+		}
 	}
 }
