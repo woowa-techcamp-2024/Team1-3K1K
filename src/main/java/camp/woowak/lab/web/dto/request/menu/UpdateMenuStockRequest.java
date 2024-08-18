14ab35d88ecd8a9933ba7f9c5fd5fb4b2c2e4976
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-18 18:07:49 +0900

    [docs] UpdateMenuStockResponse 수정 - Validate Message 추가

--- src/main/java/camp/woowak/lab/web/dto/request/menu/UpdateMenuStockRequest.java
+++ src/main/java/camp/woowak/lab/web/dto/request/menu/UpdateMenuStockRequest.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateMenuStockRequest(
-	@NotNull
+	@NotNull(message = "메뉴 ID는 필수입니다.")
 	Long menuId,
-	@Min(value = 0)
+	@Min(value = 0, message = "재고는 0 이상이어야 합니다.")
 	int stock) {
 }
