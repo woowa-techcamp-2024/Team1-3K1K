3414520a0ba68c367ff1e965ff718552db99aa94
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-18 15:38:37 +0900

    [feat] MenuRepository 수정 - MenuRepository.findByIdForUpdate: 비관적 락을 적용한 findById - MenuRepository.updateStock: 메뉴 수량 변경

--- src/main/java/camp/woowak/lab/menu/repository/MenuRepository.java
+++ src/main/java/camp/woowak/lab/menu/repository/MenuRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,17 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT m FROM Menu m where m.id in :ids")
 	List<Menu> findAllByIdForUpdate(List<Long> ids);
+
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<Menu> findByIdForUpdate(Long id);
+
+	/**
+	 *
+	 * 메뉴의 재고를 변경합니다.
+	 * TODO: [논의] @Transactional을 Respository 단에 안둬도되는가?
+	 * Repository 에서 직접 접근할 때 사용자가 실수해서 @Transactional 을 빼먹을 수도 있다.
+	 */
+	@Modifying
+	@Query("UPDATE Menu m SET m.stockCount = :stock WHERE m.id = :id")
+	int updateStock(@Param("id") Long id, @Param("stock") int stock);
 }
