16b49578080f12ff3d0afa54890349e383587b62
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-20 00:27:13 +0900

    [refactor] 생성자로 입력받은 hour와 minute을 통해 현재 날짜를 기준으로 개장시간~폐장시간을 return하도록 수정

--- src/main/java/camp/woowak/lab/store/domain/StoreTime.java
+++ src/main/java/camp/woowak/lab/store/domain/StoreTime.java
@@ -4,24 +4,48 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
 public class StoreTime {
 
 	@Column(nullable = false)
-	private LocalDateTime startTime;
-
+	private int startHour;
 	@Column(nullable = false)
-	private LocalDateTime endTime;
+	private int startMinute;
+	@Column(nullable = false)
+	private int endHour;
+	@Column(nullable = false)
+	private int endMinute;
+	@Transient
+	private boolean overDay;
 
 	public StoreTime(final LocalDateTime startTime, final LocalDateTime endTime) {
-		this.startTime = startTime;
-		this.endTime = endTime;
+		this.startHour = startTime.getHour();
+		this.startMinute = startTime.getMinute();
+
+		this.endHour = endTime.getHour();
+		this.endMinute = endTime.getMinute();
+
+		this.overDay = startTime.toLocalDate().isBefore(endTime.toLocalDate());
 	}
 
+	public LocalDateTime getStartTime() {
+		LocalDateTime now = LocalDateTime.now();
+		int year = now.getYear();
+		int month = now.getMonthValue();
+		int day = now.getDayOfMonth();
+		return LocalDateTime.of(year, month, day, startHour, startMinute, 0, 0);
+	}
+
+	public LocalDateTime getEndTime() {
+		LocalDateTime now = LocalDateTime.now().plusDays(overDay ? 1 : 0);
+		int year = now.getYear();
+		int month = now.getMonthValue();
+		int day = now.getDayOfMonth();
+		return LocalDateTime.of(year, month, day, endHour, endMinute, 0, 0);
+	}
 }
