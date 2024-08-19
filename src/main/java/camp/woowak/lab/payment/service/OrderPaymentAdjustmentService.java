afbd102ed632823ea69e83569ba3791f14d9728e
june-777 <wlwhswnsrl96@gmail.com>
2024-08-19 21:12:53 +0900

    [feat] OrderPayment 정산 서비스 구현

--- src/main/java/camp/woowak/lab/payment/service/OrderPaymentAdjustmentService.java
+++ src/main/java/camp/woowak/lab/payment/service/OrderPaymentAdjustmentService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import camp.woowak.lab.menu.repository.MenuRepository;
-import camp.woowak.lab.order.domain.vo.OrderItem;
 import camp.woowak.lab.payment.domain.OrderPayment;
 import camp.woowak.lab.payment.domain.OrderPaymentStatus;
 import camp.woowak.lab.payment.repository.OrderPaymentRepository;
@@ -23,29 +21,27 @@ import lombok.RequiredArgsConstructor;
 public class OrderPaymentAdjustmentService {
 
 	private final VendorRepository vendorRepository;
-	private final MenuRepository menuRepository;
 	private final OrderPaymentRepository orderPaymentRepository;
+	private final AdjustmentCalculator adjustmentCalculator;
 
 	/**
 	 * @throws NotFoundVendorException vendorId에 해당하는 점주를 찾을 수 없을 떄
 	 */
 	@Transactional
 	public void adjustment() {
-		// 1. 모든 점주 조회
+		// 모든 점주 조회
 		List<Vendor> vendors = findAllVendors();
 		for (Vendor vendor : vendors) {
-			// 2. 각 점주의 정산해야할 OrderPayment 목록을 조회
+			// 각 점주의 정산해야할 OrderPayment 목록을 조회
 			List<OrderPayment> orderPayments = findOrderPaymentsToAdjustment(vendor);
 
-			// 3. 각 OrderPayment 의 Order 주문 금액을 계산
-			Long totalOrderPrice = calculateTotalOrderPrice(orderPayments);
+			// 수수료를 제외한 정산 금액을 계산
+			Long totalAdjustmentPrice = adjustmentCalculator.calculate(orderPayments);
 
-			// 4. 총 주문 금액에서 수수료 5%를 계산
-			Double commission = calculateCommission(totalOrderPrice);
+			// 정산금을 점주에게 송금
+			vendor.getPayAccount().deposit(totalAdjustmentPrice);
 
-			// 5. 수수료를 제외한 금액을 점주에게 송금
-
-			// 6. 송금을 성공하면, OrderPayment 목록의 OrderPaymentStatus 상태를 ADJUSTMENT_SUCCESS 로 갱신
+			// 송금을 성공하면, OrderPayment 목록의 OrderPaymentStatus 상태를 ADJUSTMENT_SUCCESS 로 갱신
 			updateOrderPaymentStatus(orderPayments);
 		}
 	}
