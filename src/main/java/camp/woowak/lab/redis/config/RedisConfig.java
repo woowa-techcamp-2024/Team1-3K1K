73ff938075fa7f30fa03d10d8f79d3aed17e9113
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-21 00:41:18 +0900

    [feat] redis관련 config파일 생성

---
+++ src/main/java/camp/woowak/lab/redis/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package camp.woowak.lab.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@Slf4j
+public class RedisConfig {
+	@Value("${spring.data.redis.port}")
+	private int redisPort;
+
+	@Value("${spring.data.redis.host}")
+	private String redisHost;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+
+		template.setKeySerializer(new StringRedisSerializer());
+
+		return template;
+	}
+}
