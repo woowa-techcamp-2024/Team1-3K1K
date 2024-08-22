c6a11d81353f69f796ca35b7953896d2a92c632f
june-777 <wlwhswnsrl96@gmail.com>
2024-08-22 21:59:12 +0900

    [feat] 비관적락 없이 아이디를 기반으로 할인 쿠폰을 찾는 기능 구현
    - 분산락을 적용한 할인 쿠폰 발급 메서드에서 사용하기 위함
    
    Co-authored-by: kimhyun5u <22kimhyun5u@gmail.com>

--- src/main/java/camp/woowak/lab/coupon/repository/CouponRepository.java
+++ src/main/java/camp/woowak/lab/coupon/repository/CouponRepository.java
@@ -13,4 +13,7 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
 	@Lock(LockModeType.PESSIMISTIC_WRITE)
 	@Query("SELECT c FROM Coupon c WHERE c.id = :id")
 	Optional<Coupon> findByIdWithPessimisticLock(Long id);
+
+	@Query("SELECT c FROM Coupon c WHERE c.id = :id")
+	Optional<Coupon> findById(Long id);
 }
