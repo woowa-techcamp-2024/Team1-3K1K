164c6aa4c73b968505ecbf85298f9b742064ba6e
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-22 21:54:50 +0900

    [feat] getter 및 생성자 추가

--- src/main/java/camp/woowak/lab/web/dto/response/CartResponse.java
+++ src/main/java/camp/woowak/lab/web/dto/response/CartResponse.java
@@ -3,11 +3,24 @@ package camp.woowak.lab.web.dto.response;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Getter;
+
+@Getter
 public class CartResponse {
 	private Long storeId;
 	private String storeName;
 	private Integer minOrderPrice;
 	private List<CartItemInfo> menus = new ArrayList<>();
+
+	public CartResponse() {
+	}
+
+	public CartResponse(Long storId, String storeName, Integer minOrderPrice, List<CartItemInfo> menus) {
+		this.storeId = storId;
+		this.storeName = storeName;
+		this.minOrderPrice = minOrderPrice;
+		this.menus = menus;
+	}
 
 	public static class CartItemInfo {
 		private Long menuId;
