f4dc74d2c4acba248f3a52a8b1af3b1dd5666358
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-23 09:32:59 +0900

    [feat] ViewController 수정 - 가게 상세 페이지 핸들러 구현

--- src/main/java/camp/woowak/lab/web/view/ViewController.java
+++ src/main/java/camp/woowak/lab/web/view/ViewController.java
@@ -1,13 +1,22 @@
 package camp.woowak.lab.web.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import camp.woowak.lab.store.service.StoreDisplayService;
+import camp.woowak.lab.store.service.response.StoreDisplayResponse;
+
 @Controller
 @RequestMapping("/view")
 public class ViewController {
+	private StoreDisplayService storeDisplayService;
+
+	public ViewController(StoreDisplayService storeDisplayService) {
+		this.storeDisplayService = storeDisplayService;
+	}
 
 	@GetMapping("/login")
 	public String login() {
@@ -20,7 +29,9 @@ public class ViewController {
 	}
 
 	@GetMapping("/stores/{id}")
-	public String store(@PathVariable Long id) {
+	public String store(@PathVariable Long id, Model model) {
+		StoreDisplayResponse storeDisplayResponse = storeDisplayService.displayStore(id);
+		model.addAttribute("store", storeDisplayResponse);
 		return "store";
 	}
 }
