73ed22b2e79e5a6245947bed34c31c5d704d4900
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-25 00:47:59 +0900

    [feat] StockRequester 수정 - StockRequester.request: 변경된 재고를 롤백하기 위해 request 에서 성공한 변경 사항 반환하도록 수정 - StockRequester.rollback: 카트 아이템 리스트를 받아서 다시 재고 수량을 롤백하는 기능 구현
    Co-authored-by: june-777 <wlwhswnsrl96@gmail.com>

--- src/main/java/camp/woowak/lab/order/domain/StockRequester.java
+++ src/main/java/camp/woowak/lab/order/domain/StockRequester.java
@@ -1,23 +1,54 @@
 package camp.woowak.lab.order.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 import camp.woowak.lab.cart.domain.vo.CartItem;
+import camp.woowak.lab.cart.exception.MenuNotFoundException;
+import camp.woowak.lab.infra.cache.MenuStockCacheService;
+import camp.woowak.lab.infra.cache.exception.CacheMissException;
 import camp.woowak.lab.menu.domain.Menu;
 import camp.woowak.lab.menu.repository.MenuRepository;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 public class StockRequester {
 	private final MenuRepository menuRepository;
+	private final MenuStockCacheService menuStockCacheService;
 
-	public StockRequester(MenuRepository menuRepository) {
+	public StockRequester(MenuRepository menuRepository, MenuStockCacheService menuStockCacheService) {
 		this.menuRepository = menuRepository;
+		this.menuStockCacheService = menuStockCacheService;
 	}
 
-	public void request(List<CartItem> cartItems) {
+	public List<CartItem> request(List<CartItem> cartItems) {
+		List<CartItem> stockDecreaseSuccessCartItems = new ArrayList<>();
+
+		// redis cache에 메뉴 재고를 요청
+		for (CartItem cartItem : cartItems) {
+			Long menuId = cartItem.getMenuId();
+			try {
+				menuStockCacheService.addAtomicStock(menuId, -cartItem.getAmount());
+				stockDecreaseSuccessCartItems.add(cartItem);
+			} catch (CacheMissException e) {// redis cache miss 면 findById 로 재고 확인 update
+				menuStockCacheService.doWithMenuIdLock(menuId, () -> {
+					Menu menu = menuRepository.findById(menuId)
+						.orElseThrow(() -> new MenuNotFoundException("메뉴가 존재하지 않습니다."));
+					menuStockCacheService.updateStock(menuId, menu.getStockCount());
+				});
+
+			} catch (Exception e) {
+				log.error("menuId: {} 재고 감소에 실패했습니다.", menuId, e);
+				return stockDecreaseSuccessCartItems;
+			}
+
+		}
+
 		List<Long> cartItemMenuIds = extractMenuIds(cartItems);
+
 		List<Menu> allById = menuRepository.findAllByIdForUpdate(cartItemMenuIds);
 		for (Menu menu : allById) {
 			for (CartItem cartItem : cartItems) {
@@ -26,9 +57,17 @@ public class StockRequester {
 				}
 			}
 		}
+
+		return stockDecreaseSuccessCartItems;
 	}
 
 	private List<Long> extractMenuIds(List<CartItem> cartItems) {
 		return cartItems.stream().map(CartItem::getMenuId).toList();
+	}
+
+	public void rollback(List<CartItem> stockDecreaseSuccessCartItems) {
+		for (CartItem cartItem : stockDecreaseSuccessCartItems) {
+			menuStockCacheService.addAtomicStock(cartItem.getMenuId(), cartItem.getAmount());
+		}
 	}
 }
