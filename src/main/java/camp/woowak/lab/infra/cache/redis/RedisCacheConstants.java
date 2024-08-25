41a8caf24e23e9306c5a83b119f8f6de75e6a36b
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-25 11:19:56 +0900

    [feat] RedisCacheConstants 수정
    - MENU_PREFIX 추가
    
    Co-authored-by: june-777 <wlwhswnsrl96@gmail.com>

--- src/main/java/camp/woowak/lab/infra/cache/redis/RedisCacheConstants.java
+++ src/main/java/camp/woowak/lab/infra/cache/redis/RedisCacheConstants.java
@@ -4,9 +4,10 @@ import java.util.concurrent.TimeUnit;
 
 public final class RedisCacheConstants {
 	public static final String MENU_STOCK_PREFIX = "menu-stock:";
-	public static final String LOCK_PREFIX = "lock:";
+	public static final String LOCK_PREFIX = "lock-";
 	public static final int RETRY_COUNT = 10;
 	public static final long LOCK_WAIT_TIME = 10L;
 	public static final long LOCK_LEASE_TIME = 3000L;
 	public static final TimeUnit LOCK_TIME_UNIT = TimeUnit.MICROSECONDS;
+	public static final String MENU_PREFIX = "menu:";
 }
