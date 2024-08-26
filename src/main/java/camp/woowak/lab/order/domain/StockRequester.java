1873aa1b279b93d8a0b4cff4b55ca50142e741f5
kimhyun5u <22kimhyun5u@gmail.com>
2024-08-26 14:42:51 +0900

    [fix] StockRequester 수정 - 재고 부족 시 성공 상품에 대한 레디스 롤백 기능 추가 - Redis 에서 검증된 상품에 대해서만 DB 싱크
    Co-authored-by: june-777 <wlwhswnsrl96@gmail.com>

--- src/main/java/camp/woowak/lab/order/domain/StockRequester.java
+++ src/main/java/camp/woowak/lab/order/domain/StockRequester.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import camp.woowak.lab.cart.domain.vo.CartItem;
 import camp.woowak.lab.cart.exception.MenuNotFoundException;
@@ -30,10 +31,15 @@ public class StockRequester {
 	 *
 	 * @throw NotEnoughStockException 재고가 부족한 경우
 	 */
+	@Transactional
 	public List<CartItem> request(List<CartItem> cartItems) {
 		List<CartItem> stockDecreaseSuccessCartItems = new ArrayList<>();
 
+		// 메뉴 한개씩 돌면서 레디스 확인하고 디비 바꿔주고
+		// 레디스에 모두 돌고 모두 성공하면 한번에 디비에 업데이트
 		// redis cache에 메뉴 재고를 요청
+		List<Long[]> menuIdAndNewStocks = new ArrayList<>();
+
 		for (CartItem cartItem : cartItems) {
 			Long menuId = cartItem.getMenuId();
 			Long newStock = null;
@@ -41,6 +47,7 @@ public class StockRequester {
 				try {
 					newStock = menuStockCacheService.addAtomicStock(menuId, -cartItem.getAmount());
 					stockDecreaseSuccessCartItems.add(cartItem);
+					menuIdAndNewStocks.add(new Long[] {menuId, newStock});
 					break; // 재고 감소 성공 시 탈출
 				} catch (CacheMissException e) { // redis cache miss 면 findById 로 재고 확인 update
 					// (1) cache miss 시 RDB 에 접근해서 재고를 가져와야해요
@@ -49,18 +56,20 @@ public class StockRequester {
 						.orElseThrow(() -> new MenuNotFoundException("메뉴가 존재하지 않습니다."));    // 준기: 10, 현수: 8 -> redis: 8
 					menuStockCacheService.updateStock(menuId, menu.getStockCount());    // lock
 				} catch (NotEnoughStockException e) {
+					log.error("menuId: {} 재고가 부족합니다.", menuId, e);
+					rollback(stockDecreaseSuccessCartItems);
 					throw e;
 				} catch (Exception e) {
 					log.error("menuId: {} 재고 감소에 실패했습니다.", menuId, e);
 					return stockDecreaseSuccessCartItems;
-				} finally {
-					if (newStock != null) {
-						menuRepository.decreaseStock(menuId, newStock);
-					}
 				}
 			}
 		}
 		// TODO: RDB 비동기 재고 업데이트
+
+		for (Long[] menuIdAndNewStock : menuIdAndNewStocks) {
+			menuRepository.updateStock(menuIdAndNewStock[0], menuIdAndNewStock[1].intValue());
+		}
 
 		return stockDecreaseSuccessCartItems;
 	}
