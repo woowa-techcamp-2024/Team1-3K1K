be96bc4b95ba72fd5207c4646da56034dd7d860d
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-27 21:45:09 +0900

    [feat] 멱등성키에 대한 확인을 할 수 있는 aop 구현
    Co-authored-by: Dr-KoKo <97681286+dr-koko@users.noreply.github.com>

---
+++ src/main/java/camp/woowak/lab/infra/aop/idempotent/IdempotentAop.java
@@ -0,0 +1,40 @@
+package camp.woowak.lab.infra.aop.idempotent;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class IdempotentAop {
+	private static final String REDIS_IDEMPOTENT_KEY = "IDEMPOTENT_KEY: ";
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	@Around("@annotation(camp.woowak.lab.infra.aop.idempotent.Idempotent)")
+	public Object idempotentOperation(ProceedingJoinPoint joinPoint) throws Throwable {
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		String idempotencyKey = request.getHeader("Idempotency-Key");
+
+		if (idempotencyKey == null || idempotencyKey.isEmpty()) {
+			throw new IllegalArgumentException("Idempotency-Key is required");
+		}
+
+		if (Boolean.TRUE.equals(redisTemplate.hasKey(REDIS_IDEMPOTENT_KEY + idempotencyKey))) {
+			return redisTemplate.opsForValue().get(REDIS_IDEMPOTENT_KEY);
+		}
+
+		Object proceed = joinPoint.proceed();
+		redisTemplate.opsForValue().set(REDIS_IDEMPOTENT_KEY + idempotencyKey, proceed);
+		return proceed;
+	}
+}
