28f9e47af9beab65e46a63b5280da299f23330dd
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-08-28 16:16:56 +0900

    [feat] 멱등한 요청에 대해 exception을 던질지, 기존의 응답을 던질지 선택할 수 있도록 어노테이션 파라미터 추가 및 적용
    Co-authored-by: Dr-KoKo <97681286+dr-koko@users.noreply.github.com>

--- src/main/java/camp/woowak/lab/infra/aop/idempotent/IdempotentAop.java
+++ src/main/java/camp/woowak/lab/infra/aop/idempotent/IdempotentAop.java
@@ -1,8 +1,13 @@
 package camp.woowak.lab.infra.aop.idempotent;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.time.Duration;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -25,17 +30,29 @@ public class IdempotentAop {
 	public Object idempotentOperation(ProceedingJoinPoint joinPoint) throws Throwable {
 		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
 		String idempotencyKey = request.getHeader("Idempotency-Key");
+		MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
+		Method method = methodSignature.getMethod();
+		Idempotent idempotent = method.getAnnotation(Idempotent.class);
 
 		if (idempotencyKey == null || idempotencyKey.isEmpty()) {
 			throw new IdempotencyKeyNotExistsException("Idempotency-Key is required");
 		}
 
 		if (Boolean.TRUE.equals(redisTemplate.hasKey(REDIS_IDEMPOTENT_KEY + idempotencyKey))) {
+			if (idempotent.throwError()) {
+				Class<? extends RuntimeException> throwable = idempotent.throwable();
+				String exceptionMessage = idempotent.exceptionMessage();
+				Constructor<? extends RuntimeException> constructor = throwable.getConstructor(String.class);
+				RuntimeException exceptions = constructor.newInstance(exceptionMessage);
+
+				throw exceptions;
+			}
 			return redisTemplate.opsForValue().get(REDIS_IDEMPOTENT_KEY + idempotencyKey);
 		}
 
 		Object proceed = joinPoint.proceed();
-		redisTemplate.opsForValue().set(REDIS_IDEMPOTENT_KEY + idempotencyKey, proceed);
+		redisTemplate.opsForValue()
+			.setIfAbsent(REDIS_IDEMPOTENT_KEY + idempotencyKey, proceed, Duration.ofSeconds(60L));
 		return proceed;
 	}
 }
