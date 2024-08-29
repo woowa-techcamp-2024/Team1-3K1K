12367ada91570a0b70585bd86d07d76a8a327842
donghar <donghar@naver.com>
2024-08-29 16:54:00 +0900

    [fix] 테스트시 in_memory 버전의 CartRepository를 사용하게 설정

--- src/test/java/camp/woowak/lab/cart/service/CartServiceConcurrencyTest.java
+++ src/test/java/camp/woowak/lab/cart/service/CartServiceConcurrencyTest.java
@@ -19,6 +19,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.TestPropertySources;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -46,6 +48,10 @@ import camp.woowak.lab.web.authentication.NoOpPasswordEncoder;
 import camp.woowak.lab.web.dao.store.StoreDummiesFixture;
 
 @SpringBootTest
+@TestPropertySources({
+	@TestPropertySource(properties = "cart.dao=redis"),
+	@TestPropertySource(properties = "cart.repository=redis")
+})
 @Disabled
 public class CartServiceConcurrencyTest extends StoreDummiesFixture {
 	private final Logger log = LoggerFactory.getLogger(CartServiceConcurrencyTest.class);
